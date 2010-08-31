aa5076322c85a9b251df02673fd396b61899c09d
Travis Brown <travis.brown@mail.utexas.edu>
2010-08-31 17:25:54 -0500

    Added XMLStreamWriter alternatives.

--- java/opennlp/textgrounder/util/KMLUtil.java
+++ java/opennlp/textgrounder/util/KMLUtil.java
@@ -15,6 +15,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 package opennlp.textgrounder.util;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 import opennlp.textgrounder.models.Model;
 import opennlp.textgrounder.topostructs.Coordinate;
 
@@ -24,7 +27,160 @@ import opennlp.textgrounder.topostructs.Coordinate;
  * @author tsmoon
  */
 public class KMLUtil {
+  protected static void writeWithCharacters(XMLStreamWriter w, String localName, String text)
+    throws XMLStreamException {
+    w.writeStartElement(localName);
+    w.writeCharacters(text);
+    w.writeEndElement();
+  }
 
+  public static void writeHeader(XMLStreamWriter w, String name)
+    throws XMLStreamException {
+    w.writeStartDocument("UTF-8", "1.0");
+    w.writeStartElement("kml");
+    w.writeDefaultNamespace("http://www.opengis.net/kml/2.2");
+    w.writeNamespace("gx", "http://www.google.com/kml/ext/2.2");
+    w.writeNamespace("kml", "http://www.opengis.net/kml/2.2");
+    w.writeNamespace("atom", "http://www.w3.org/2005/Atom");
+    w.writeStartElement("Document");
+    w.writeStartElement("Style");
+    w.writeAttribute("id", "bar");
+    w.writeStartElement("PolyStyle");
+    KMLUtil.writeWithCharacters(w, "outline", "0");
+    w.writeEndElement(); // PolyStyle
+    w.writeStartElement("IconStyle");
+    w.writeEmptyElement("Icon");
+    w.writeEndElement(); // IconStyle
+    w.writeEndElement(); // Style
+    w.writeStartElement("Style");
+    w.writeAttribute("id", "downArrowIcon");
+    w.writeStartElement("IconStyle");
+    w.writeStartElement("Icon");
+    KMLUtil.writeWithCharacters(w, "href", "http://maps.google.com/mapfiles/kml/pal4/icon28.png");
+    w.writeEndElement(); // Icon
+    w.writeEndElement(); // IconStyle
+    w.writeEndElement(); // Style
+    w.writeStartElement("Folder");
+    KMLUtil.writeWithCharacters(w, "name", name);
+    KMLUtil.writeWithCharacters(w, "open", "1");
+    KMLUtil.writeWithCharacters(w, "description", "Distribution of place names found in " + name);
+    KMLUtil.writeLookAt(w, 42, -102, 0, 5000000, 53.454348562403, 0);
+  }
+
+  public static void writeFooter(XMLStreamWriter w)
+    throws XMLStreamException {
+    w.writeEndElement(); // Folder
+    w.writeEndElement(); // Document
+    w.writeEndElement(); // kml
+  }
+
+  public static void writeCoordinate(XMLStreamWriter w, Coordinate coord,
+                                     int sides, double radius, double height)
+    throws XMLStreamException {
+    final double radianUnit = 2 * Math.PI / sides;
+    final double startRadian = radianUnit / 2;
+
+    w.writeStartElement("coordinates");
+    w.writeCharacters("\n");
+    for (double currentRadian = startRadian; currentRadian <= 2 * Math.PI + startRadian; currentRadian += radianUnit) {
+      double lat = coord.latitude + radius * Math.cos(currentRadian);
+      double lon = coord.longitude + radius * Math.sin(currentRadian);
+      w.writeCharacters(String.format("%f,%f,%f\n", lat, lon, height));
+    }
+
+    w.writeEndElement(); // coordinate
+  }
+
+  public static void writeRegion(XMLStreamWriter w, Coordinate coord, double radius)
+    throws XMLStreamException {
+    w.writeStartElement("Region");
+    w.writeStartElement("LatLonAltBox");
+    KMLUtil.writeWithCharacters(w, "north", String.format("%f", coord.latitude + radius));
+    KMLUtil.writeWithCharacters(w, "south", String.format("%f", coord.latitude - radius));
+    KMLUtil.writeWithCharacters(w, "east", String.format("%f", coord.longitude + radius));
+    KMLUtil.writeWithCharacters(w, "west", String.format("%f", coord.longitude - radius));
+    w.writeEndElement(); // LatLonAltBox
+    w.writeStartElement("Lod");
+    KMLUtil.writeWithCharacters(w, "minLodPixels", Integer.toString(Model.MIN_LOD_PIXELS));
+    w.writeEndElement(); // Lod
+    w.writeEndElement(); // Region
+  }
+
+  public static void writeLookAt(XMLStreamWriter w, double lat, double lon,
+                                 double alt, double range, double tilt, double heading)
+    throws XMLStreamException {
+    w.writeStartElement("LookAt");
+    KMLUtil.writeWithCharacters(w, "latitude", String.format("%f", lat));
+    KMLUtil.writeWithCharacters(w, "longitude", String.format("%f", lon));
+    KMLUtil.writeWithCharacters(w, "altitude", String.format("%f", alt));
+    KMLUtil.writeWithCharacters(w, "range", String.format("%f", range));
+    KMLUtil.writeWithCharacters(w, "tilt", String.format("%f", tilt));
+    KMLUtil.writeWithCharacters(w, "heading", String.format("%f", heading));
+    w.writeEndElement(); // LookAt
+  }
+
+  public static void writePolygon(XMLStreamWriter w, String name,
+                                  Coordinate coord, int sides, double radius, double height)
+    throws XMLStreamException {
+    w.writeStartElement("Placemark");
+    KMLUtil.writeWithCharacters(w, "name", name);
+    KMLUtil.writeRegion(w, coord, radius);
+    KMLUtil.writeWithCharacters(w, "styleUrl", "#bar");
+    w.writeStartElement("Point");
+    KMLUtil.writeWithCharacters(w, "coordinates", coord.toString());
+    w.writeEndElement(); // Point
+    w.writeEndElement(); // Placemark
+    w.writeStartElement("Placemark");
+    KMLUtil.writeWithCharacters(w, "name", name + " POLYGON");
+    KMLUtil.writeWithCharacters(w, "styleUrl", "#bar");
+    w.writeStartElement("Style");
+    w.writeStartElement("PolyStyle");
+    KMLUtil.writeWithCharacters(w, "color", "dc0155ff");
+    w.writeEndElement(); // PolyStyle
+    w.writeEndElement(); // Style
+    w.writeStartElement("Polygon");
+    KMLUtil.writeWithCharacters(w, "extrude", "1");
+    KMLUtil.writeWithCharacters(w, "tessellate", "1");
+    KMLUtil.writeWithCharacters(w, "altitudeMode", "relativeToGround");
+    w.writeStartElement("outerBoundaryIs");
+    w.writeStartElement("LinearRing");
+    KMLUtil.writeCoordinate(w, coord, sides, radius, height);
+    w.writeEndElement(); // LinearRing
+    w.writeEndElement(); // outerBoundaryIs
+    w.writeEndElement(); // Polygon
+    w.writeEndElement(); // Placemark
+  }
+
+  public static void writeSpiralPoint(XMLStreamWriter w, String name, int id,
+                                      String context, Coordinate coord, double radius)
+    throws XMLStreamException {
+    w.writeStartElement("Placemark");
+    KMLUtil.writeWithCharacters(w, "name", String.format("%s #%d", name, id + 1));
+    KMLUtil.writeWithCharacters(w, "description", context);
+    KMLUtil.writeRegion(w, coord, radius);
+    KMLUtil.writeWithCharacters(w, "styleUrl", "#context");
+    w.writeStartElement("Point");
+    KMLUtil.writeWithCharacters(w, "coordinates", coord.toString());
+    w.writeEndElement(); // Point
+    w.writeEndElement(); // Placemark
+  }
+
+  public static void writeFloatingPlacemark(XMLStreamWriter w, String name,
+                                            Coordinate coord, double height)
+    throws XMLStreamException {
+    w.writeStartElement("Placemark");
+    KMLUtil.writeWithCharacters(w, "name", name);
+    KMLUtil.writeWithCharacters(w, "visibility", "1");
+    KMLUtil.writeLookAt(w, coord.latitude, coord.longitude, height, 500.6566641072245, 40.5575073395506, -148.4122922628044);
+    KMLUtil.writeWithCharacters(w, "styleUrl", "#downArrowIcon");
+    w.writeStartElement("Point");
+    KMLUtil.writeWithCharacters(w, "altitudeMode", "relativeToGround");
+    KMLUtil.writeWithCharacters(w, "coordinates", String.format("%s,%f", coord, height));
+    w.writeEndElement(); // Point
+    w.writeEndElement(); // Placemark
+  }
+
+    @Deprecated
     public static String genKMLHeader(String inputFilename) {
         return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
               + "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n"
@@ -58,10 +214,12 @@ public class KMLUtil {
               + "\t\t\t</LookAt>\n";
     }
 
+    @Deprecated
     public static String genKMLFooter() {
         return "\t\t</Folder>\n\t</Document>\n</kml>";
     }
 
+    @Deprecated
     public static String genPolygon(String placename, Coordinate coord,
           double radius, String kmlPolygon) {
         return "\t\t\t<Placemark>\n"
@@ -100,6 +258,7 @@ public class KMLUtil {
               + "\t\t\t</Placemark>\n";
     }
 
+    @Deprecated
     public static String genSpiralpoint(String placename, String context,
           Coordinate spiralPoint, int j, double radius) {
         return "\t\t\t<Placemark>\n"
@@ -123,6 +282,7 @@ public class KMLUtil {
               + "\t\t\t</Placemark>\n";
     }
 
+    @Deprecated
     public static String genFloatingPlacemark(String word, Coordinate coord,
           double height) {
         return "<Placemark>\n"
