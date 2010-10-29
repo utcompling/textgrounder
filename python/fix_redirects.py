66c44449a695344841977f9c38b19deec026a56a
Ben Wing <ben@benwing.com>
2010-10-29 15:00:40 -0500

    fix_redirects.py: One-off script for capitalizing first letter of redirects Useful as an example of a simple script using NLPProgram

---
+++ python/fix_redirects.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+#######
+####### fix_redirects.py
+#######
+####### Copyright (c) 2010 Ben Wing.
+#######
+
+# This is a one-off program to fix the redirect fields so that they
+# always begin with a capital letter, as is required of article titles in
+# Wikipedia.
+
+import sys
+from textutil import *
+from process_article_data import *
+
+def fix_redirects(filename):
+  articles_seen = []
+  def process(art):
+    if art.redir:
+      art.redir = capfirst(art.redir)
+    articles_seen.append(art)
+  errprint("Reading from %s..." % filename)
+  fields = read_article_data_file(filename, process,
+                                  max_time_per_stage=Opts.max_time_per_stage)
+  errprint("Writing to stdout...")
+  write_article_data_file(sys.stdout, outfields=fields,
+                          articles=articles_seen)
+  errprint("Done.")
+
+############################################################################
+#                                  Main code                               #
+############################################################################
+
+class FixRedirectsProgram(NLPProgram):
+  def argument_usage(self):
+    return "article-data-file"
+
+  def implement_main(self, opts, op, args):
+    global Opts
+    Opts = opts
+    if len(args) != 1:
+      op.error("Must specify exactly one article-data file as an argument")
+    fix_redirects(args[0])
+    
+FixRedirectsProgram()
