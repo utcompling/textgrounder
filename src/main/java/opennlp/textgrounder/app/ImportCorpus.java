6286a580498d6ed243b1d1bad9c7cd4d2910283c
abhimanyu kumar <abhimanyu.kumar@gmail.com>
2011-04-15 05:21:52 -0500

    High Recall NER

--- src/main/java/opennlp/textgrounder/app/ImportCorpus.java
+++ src/main/java/opennlp/textgrounder/app/ImportCorpus.java
@@ -45,6 +45,7 @@ public class ImportCorpus extends BaseApp {
             ois = new ObjectInputStream(fis);
         }
         gnGaz = (GeoNamesGazetteer) ois.readObject();
+        recognizer = new HighRecallToponymRecognizer(gnGaz.getUniqueLocationNameSet());
         System.out.println("Done.");
 
         System.out.print("Reading raw corpus from " + corpusInputPath + " ...");
