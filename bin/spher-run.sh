1d1ba562ea089a7fa194da22cc913358b3595ce1
Taesun Moon <devnull@localhost>
2011-04-20 17:47:36 -0500

    shell script update

--- bin/spher-run.sh
+++ bin/spher-run.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-. textgrounder-env
+
+# TextGrounder-related
+TG_DIR="$TEXTGROUNDER_DIR"
+TG_LIB="$TG_DIR/target/assembly/work"
+TG_CP="$TG_DIR/target/classes:$TG_LIB/trove.jar:$TG_LIB/commons-cli-1.2.jar:$TG_LIB/jdom-1.1.jar"
+
+# Create the combined CLASSPATH
+CP="$TG_CP"
+JAVA="$JAVA_HOME/bin/java"
+
+# Determine the heap-size flag
+if [ -z "$JAVA_MEM_FLAG" ] 
+then
+    JAVA_MEM_FLAG=-Xmx2500m
+fi
+
+# Now prepares the Java command with the right settings
+JAVA_CMD="$JAVA $JAVA_MEM_FLAG -classpath $CP"
+
 $JAVA_CMD opennlp.textgrounder.bayesian.apps.TrainSphericalModel $@
