f90415b87e928ffa43889f63eadb60233fa82fc0
Mike Speriosu <speriosu@gmail.com>
2011-05-27 22:53:43 -0500

    Added bigram nodes to GeoText label propagation preprocessing.

--- src/main/scala/opennlp/textgrounder/app/GeoTextLabelPropPreproc.scala
+++ src/main/scala/opennlp/textgrounder/app/GeoTextLabelPropPreproc.scala
@@ -15,6 +15,8 @@ object GeoTextLabelPropPreproc extends BaseApp {
 
   import BaseApp._
 
+  def MIN_COUNT_THRESHOLD = 5
+
   def DPC = 1.0
 
   def CELL_ = "cell_"
@@ -73,16 +75,69 @@ object GeoTextLabelPropPreproc extends BaseApp {
   }
 
   def writeNGramDocEdges(corpus: StoredCorpus, graphOutputPath: String, stoplist: Set[String]) = {
-    val out = new FileWriter(graphOutputPath, true)
+
+    val docIdsToNGrams = new collection.mutable.HashMap[String, collection.mutable.HashSet[String]]
+    val unigramsToCounts = new collection.mutable.HashMap[String, Int]
+    val bigramsToCounts = new collection.mutable.HashMap[String, Int]
 
     for(document <- corpus) {
+      docIdsToNGrams.put(document.getId, new collection.mutable.HashSet[String]())
       for(sentence <- document) {
+        var prevUni:String = null
         for(token <- sentence) {
-          if(!stoplist.contains(token.getForm))
-            writeEdge(out, UNI_ + token.getForm, document.getId, 1.0)
+          val unigram = token.getForm
+          if(!stoplist.contains(unigram)) {
+            docIdsToNGrams.get(document.getId).get += UNI_ + unigram
+            if(!unigramsToCounts.contains(unigram)) {
+              unigramsToCounts.put(unigram, 1)
+              //println("saw " + unigram + " for the first time.")
+            }
+            else {
+              unigramsToCounts.put(unigram, unigramsToCounts.get(unigram).get + 1)
+              //println("saw " + unigram + " for the " + (unigramsToCounts.get(unigram).get + 1) + " time.")
+            }
+          }
+          if(prevUni != null) {
+            val bigram = prevUni + " " + unigram
+            docIdsToNGrams.get(document.getId).get += BI_ + bigram
+            if(!bigramsToCounts.contains(bigram))
+              bigramsToCounts.put(bigram, 1)
+            else
+              bigramsToCounts.put(bigram, bigramsToCounts.get(bigram).get + 1)
+          }
+          prevUni = unigram
         }
       }
     }
+
+    val out = new FileWriter(graphOutputPath, true)
+
+    for(docId <- docIdsToNGrams.keys) {
+      for(ngram <- docIdsToNGrams.get(docId).get) {
+        val shortNGram =
+          if(ngram.startsWith(BI_)) ngram.substring(BI_.length)
+          else if(ngram.startsWith(UNI_)) ngram.substring(UNI_.length)
+          else ngram
+        if((unigramsToCounts.contains(shortNGram) && unigramsToCounts.get(shortNGram).get >= MIN_COUNT_THRESHOLD)
+           || (bigramsToCounts.contains(shortNGram) && bigramsToCounts.get(shortNGram).get >= MIN_COUNT_THRESHOLD)) {
+          writeEdge(out, ngram, docId, 1.0)
+          //println("writing " + ngram + " " + docId)
+           }
+      }
+    }
+
+    /*for(document <- corpus) {
+      for(sentence <- document) {
+        var prevUni:String = null
+        for(token <- sentence) {
+          if(!stoplist.contains(token.getForm))
+            writeEdge(out, UNI_ + token.getForm, document.getId, 1.0)
+          if(prevUni != null)
+            writeEdge(out, BI_ + prevUni + " " + token.getForm, document.getId, 1.0)
+          prevUni = token.getForm
+        }
+      }
+    }*/
 
     out.close
   }
