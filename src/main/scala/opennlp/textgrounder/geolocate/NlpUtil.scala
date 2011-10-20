6ebc20fc35705515cea260a5d460fecad8f4f500
Ben Wing <ben@benwing.com>
2011-10-19 23:37:39 -0500

    Also output virtual memory size -- and output actual memory-sizing method when 'auto' requested

--- src/main/scala/opennlp/textgrounder/geolocate/NlpUtil.scala
+++ src/main/scala/opennlp/textgrounder/geolocate/NlpUtil.scala
@@ -1110,20 +1110,24 @@ object NlpUtil {
   
   def get_program_time_usage() = curtimesecs() - beginning_prog_time
 
-  def get_program_memory_usage(method: String = "auto"): Long = {
+  /**
+   * Return memory usage as a 
+   */
+  def get_program_memory_usage(virtual: Boolean = false,
+      method: String = "auto"): (String, Long) = {
     method match {
-      case "java" => get_program_memory_usage_java()
-      case "proc" => get_program_memory_usage_proc()
-      case "ps" => get_program_memory_usage_ps()
-      case "rusage" => get_program_memory_usage_rusage()
+      case "java" => (method, get_program_memory_usage_java())
+      case "proc" => (method, get_program_memory_usage_proc(virtual = virtual))
+      case "ps" => (method, get_program_memory_usage_ps(virtual = virtual))
+      case "rusage" => (method, get_program_memory_usage_rusage())
       case "auto" => {
-        val procmem = get_program_memory_usage_proc()
-        if (procmem > 0) return procmem
-        val psmem = get_program_memory_usage_ps()
-        if (psmem > 0) return psmem
+        val procmem = get_program_memory_usage_proc(virtual = virtual)
+        if (procmem > 0) return ("proc", procmem)
+        val psmem = get_program_memory_usage_ps(virtual = virtual)
+        if (psmem > 0) return ("ps", psmem)
         val rusagemem = get_program_memory_usage_rusage()
-        if (rusagemem > 0) return rusagemem
-        return get_program_memory_usage_java()
+        if (rusagemem > 0) return ("rusage", rusagemem)
+        return ("java", get_program_memory_usage_java())
       }
     }
   }
@@ -1154,30 +1158,37 @@ object NlpUtil {
 
   // Get memory usage by running 'ps'; getrusage() doesn't seem to work very
   // well.  The following seems to work on both Mac OS X and Linux, at least.
-  def get_program_memory_usage_ps(wraperr: Boolean = true): Long = {
+  def get_program_memory_usage_ps(virtual: Boolean = false,
+      wraperr: Boolean = true): Long = {
     if (wraperr)
-      return wrap_call(get_program_memory_usage_ps(wraperr=false), -1L)
+      return wrap_call(get_program_memory_usage_ps(
+        virtual=virtual, wraperr=false), -1L)
+    val header = if (virtual) "vsz" else "rss"
     val pid = getpid()
     val input =
-      capture_subprocess_output("ps", "-p", pid.toString, "-o", "rss")
+      capture_subprocess_output("ps", "-p", pid.toString, "-o", header)
     val lines = input.split('\n')
-    for (line <- lines if line.trim != "RSS")
+    for (line <- lines if line.trim != header.toUpperCase)
       return 1024*line.trim.toLong
     return -1L
   }
  
-  // Get memory usage by running 'proc'; this works on Linux and doesn't require
-  // spawning a subprocess, which can crash when your program is very large.
-  def get_program_memory_usage_proc(wraperr: Boolean = true): Long = {
+  // Get memory usage by running 'proc'; this works on Linux and doesn't
+  // require spawning a subprocess, which can crash when your program is
+  // very large.
+  def get_program_memory_usage_proc(virtual: Boolean = false,
+      wraperr: Boolean = true): Long = {
     if (wraperr)
-      return wrap_call(get_program_memory_usage_proc(wraperr=false), -1L)
+      return wrap_call(get_program_memory_usage_proc(
+        virtual=virtual, wraperr=false), -1L)
+    val header = if (virtual) "VmSize:" else "VmRSS:"
     if (!((new File("/proc/self/status")).exists))
       return -1L
     for (line <- openr("/proc/self/status")) {
         val trimline = line.trim
-        if (trimline.startsWith("VmRSS:")) {
-          val rss = ("""\s+""".r.split(trimline))(1).toLong
-          return 1024*rss
+        if (trimline.startsWith(header)) {
+          val size = ("""\s+""".r.split(trimline))(1).toLong
+          return 1024*size
         }
       }
     return -1L
@@ -1199,10 +1210,13 @@ object NlpUtil {
         secstr, if (secs == 1) "" else "s")
   }
   
-  def output_memory_usage() {
+  def output_memory_usage(virtual: Boolean = false) {
     for (method <- List("auto", "java", "proc", "ps", "rusage")) {
-      val mem = get_program_memory_usage(method)
-      errout("Memory usage (%s): ", method)
+      val (meth, mem) =
+        get_program_memory_usage(virtual = virtual, method = method)
+      val memtype = if (virtual) "virtual size" else "resident set size"
+      val methstr = if (method == "auto") "auto=%s" format meth else method
+      errout("Memory usage, %s (%s): ", memtype, methstr)
       if (mem <= 0)
         errprint("Unknown")
       else
@@ -1213,11 +1227,18 @@ object NlpUtil {
   def output_resource_usage(dojava: Boolean = true) {
     errprint("Total elapsed time since program start: %s",
              format_minutes_seconds(get_program_time_usage()))
-    errprint("Memory usage (auto): %s bytes",
-        long_with_commas(get_program_memory_usage("auto")))
+    val (vszmeth, vsz) = get_program_memory_usage(virtual = true,
+      method = "auto")
+    errprint("Memory usage, virtual memory size (%s): %s bytes", vszmeth,
+      long_with_commas(vsz))
+    val (rssmeth, rss) = get_program_memory_usage(virtual = false,
+      method = "auto")
+    errprint("Memory usage, actual (i.e. resident set) (%s): %s bytes", rssmeth,
+      long_with_commas(rss))
     if (dojava) {
-      errprint("Memory usage (java): %s bytes",
-          long_with_commas(get_program_memory_usage("java")))
+      val (_, java) = get_program_memory_usage(virtual = false,
+        method = "java")
+      errprint("Memory usage, Java heap: %s bytes", long_with_commas(java))
     } else
       System.gc()
   }
