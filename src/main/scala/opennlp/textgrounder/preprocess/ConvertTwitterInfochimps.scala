286d7c22455482c09ce8e84cf50b06f5093fea04
Ben Wing <ben@benwing.com>
2011-11-23 00:44:47 -0700

    Rethink the format of data, esp. to make it easier to handle corpora from multiple sources and to run through Hadoop; create a new 'doc' directory holding documentation and add to it a file I wrote on the new formats; commit partially-written code to convert the data appropriately into these new formats and compute statistics on the data

--- src/main/scala/opennlp/textgrounder/preprocess/ConvertTwitterInfochimps.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/ConvertTwitterInfochimps.scala
@@ -16,11 +16,12 @@
 
 package opennlp.textgrounder.preprocess
 
-import java.io.InputStream
+import java.io.{InputStream, PrintStream}
 
 import org.apache.commons.lang3.StringEscapeUtils._
 
 import opennlp.textgrounder.util.argparser._
+import opennlp.textgrounder.util.collectionutil._
 import opennlp.textgrounder.util.experiment._
 import opennlp.textgrounder.util.ioutil._
 import opennlp.textgrounder.util.MeteredTask
@@ -67,46 +68,27 @@ The fields are:
 //                                  Main code                              //
 /////////////////////////////////////////////////////////////////////////////
 
-class ConvertTwitterInfochimpsParameters(parser: ArgParser) extends
-    ProcessFilesParameters(parser) {
-  var group_by_user =
-    ap.flag("group-by-user",
-      help = """If true, prepare for grouping tweets into a single per-user document.""")
+class ConvertTwitterInfochimpsParameters(ap: ArgParser) extends
+    ArgParserParameters(ap) {
+  val output_dir =
+    ap.option[String]("o", "output-dir",
+      metavar = "DIR",
+      help = """Directory to store output files in.""")
+  val output_stats =
+    ap.flag("output-stats",
+      help = """If true, output statistics on the tweets in the input,
+rather than converting the files.""")
+  val files =
+    ap.multiPositional[String]("files",
+      help = """File(s) to process for input.""")
 }
 
-class ConvertTwitterInfochimpsDriver extends ProcessFilesDriver {
-  type ParamType = ConvertTwitterInfochimpsParameters
-  
-  def usage() {
-    sys.error("""Usage: ConvertTwitterInfochimps [-o OUTDIR | --outfile OUTDIR] [--group-by-user] INFILE ...
-
-Convert input files in the Infochimps Twitter corpus into files in the
-format expected by TextGrounder.  OUTDIR is the directory to store the
-results in, which must not exist already.  If --group-by-user is given,
-output an extra field in the text file containing the user, to make it
-easier to group tweets by user.
-""")
-  }
-
-  override def run_after_setup() {
-    val fields = List("id", "title", "split", "coord", "time",
-      "username", "userid", "reply_username", "reply_userid", "anchor", "lang")
-    // FIXME: Output fields as a schema file
-    super.run_after_setup()
-  }
-
-  def process_one_file(lines: Iterator[String], outname: String) {
-    val compression_type = "bzip2"
+abstract class TwitterInfochimpsFileProcessor extends TextFileProcessor {
+  def process_lines(lines: Iterator[String],
+      filehand: FileHandler, file: String,
+      compression: String, realname: String) = {
     val task = new MeteredTask("tweet", "parsing")
-    val out_metadata_name =
-      "%s/%s-combined-document-data.txt" format (params.output_dir, outname)
-    val out_text_name = "%s/%s-text.txt" format (params.output_dir, outname)
-    errprint("Metadata output file is %s..." format out_metadata_name)
-    errprint("Text output file is %s..." format out_text_name)
-    val outstream_metadata = filehand.openw(out_metadata_name,
-      compression = compression_type)
-    val outstream_text = filehand.openw(out_text_name,
-      compression = compression_type)
+    begin_process_file(filehand, file, compression, realname)
     var lineno = 0
     for (line <- lines) {
       lineno += 1
@@ -114,19 +96,11 @@ easier to group tweets by user.
         case id :: time :: userid :: username :: _ ::
             reply_username :: reply_userid :: _ :: text :: anchor :: lang ::
             lat :: long :: _ :: _ :: _ :: _ :: _ :: Nil => {
-          val title = "Twitter:" + id
-          val metadata = List(id, title, "training",
+          val title = id
+          val metadata = Seq(id, title, "training",
             "%s,%s" format (lat, long), time, username, userid,
             reply_username, reply_userid, anchor, lang)
-          outstream_metadata.println(metadata mkString "\t")
-          val rawtext = unescapeHtml4(unescapeXml(text))
-          val splittext = Twokenize(rawtext)
-          val textdata =
-            if (params.group_by_user)
-              List(username, title, splittext mkString " ")
-            else
-              List(title, splittext mkString " ")
-          outstream_text.println(textdata mkString "\t")
+          process_line(metadata, text)
         }
         case _ => {
           errprint("Bad line #%d: %s" format (lineno, line))
@@ -135,9 +109,197 @@ easier to group tweets by user.
       }
       task.item_processed()
     }
-    outstream_text.close()
-    outstream_metadata.close()
+    end_process_file(filehand, file)
     task.finish()
+    true
+  }
+
+  // To be implemented
+
+  def begin_process_file(filehand: FileHandler, file: String,
+      compression: String, realname: String)
+
+  def process_line(metadata: Seq[String], text: String)
+
+  def end_process_file(filehand: FileHandler, file: String)
+}
+
+class ConvertTwitterInfochimpsFileProcessor(
+  params: ConvertTwitterInfochimpsParameters
+) extends TwitterInfochimpsFileProcessor {
+  var outstream: PrintStream = _
+  val compression_type = "bzip2"
+
+  def begin_process_file(filehand: FileHandler, file: String,
+      compression: String, realname: String) {
+    val (_, outname) = filehand.split_filename(realname)
+    val out_text_name = "%s/%s-text.txt" format (params.output_dir, outname)
+    errprint("Text output file is %s..." format out_text_name)
+    outstream = filehand.openw(out_text_name, compression = compression_type)
+  }
+
+  def process_line(metadata: Seq[String], text: String) {
+    val rawtext = unescapeHtml4(unescapeXml(text))
+    val splittext = Twokenize(rawtext)
+    val outdata = metadata ++ Seq(splittext mkString " ")
+    outstream.println(outdata mkString "\t")
+  }
+
+  def end_process_file(filehand: FileHandler, file: String) {
+    outstream.close()
+  }
+}
+
+class TwitterInfochimpsStatsFileProcessor extends
+    TwitterInfochimpsFileProcessor {
+  class TwitterStatistics {
+    // Over-all statistics
+    var num_tweets = 0
+
+    // Time-based statistics
+    val tweets_by_day = intmap[String]()
+    val tweets_by_hour = intmap[String]()
+    val tweets_by_minute = intmap[String]()
+    // In general, when finding the minimum, we want the default to be greater
+    // than any possible value, and when finding the maximum, we want the
+    // default to be less than any possible value.  For string comparison,
+    // the empty string is less than any other string, and a string beginning
+    // with Unicode 0xFFFF is greater than almost any other string. (0xFFFF
+    // is not a valid Unicode character.)
+    var earliest_tweet_time = "\uFFFF"
+    var latest_tweet_time = ""
+    var earliest_tweet_id = "\uFFFF"
+    var latest_tweet_id = ""
+
+    // If tweet ID's are sorted by time, the following two should be
+    // identical to the earliest_tweet_id/latest_tweet_id.
+    var lowest_tweet_id = "\uFFFF"
+    var highest_tweet_id = ""
+
+    // User-based statistics
+    val tweets_by_user = intmap[String]()
+    val userid_by_user = setmap[String, String]()
+    val reply_userid_by_reply_user = setmap[String, String]()
+    val reply_user_by_user = setmap[String, String]()
+
+    def record_tweet(metadata: Seq[String], text: String) {
+      assert(metadata.length == 11)
+      val Seq(id, title, split, coords, time, username, userid, reply_username,
+        reply_userid, anchor, lang) = metadata
+
+      num_tweets += 1
+
+      // Time format is YYYYMMDDHHmmSS, hence take(8) goes up through the day,
+      // take(10) up through the hour, etc.
+      tweets_by_day(time.take(8)) += 1
+      tweets_by_hour(time.take(10)) += 1
+      tweets_by_minute(time.take(12)) += 1
+
+      if (time < earliest_tweet_time) {
+        earliest_tweet_time = time
+        earliest_tweet_id = id
+      }
+      if (time > latest_tweet_time) {
+        latest_tweet_time = time
+        latest_tweet_id = id
+      }
+      if (id < lowest_tweet_id)
+        lowest_tweet_id = id
+      if (id > highest_tweet_id)
+        highest_tweet_id = id
+
+      tweets_by_user(username) += 1
+      userid_by_user(username) += userid
+      if (reply_username != "") {
+        reply_userid_by_reply_user(reply_username) += reply_userid
+        reply_user_by_user(username) += reply_username
+      }
+    }
+
+    def print_statistics() {
+      errprint("Number of tweets: %s" format num_tweets)
+      errprint("Earliest tweet: %s at %s" format
+        (earliest_tweet_id, earliest_tweet_time))
+      errprint("Lowest tweet ID: %s" format lowest_tweet_id)
+      errprint("Latest tweet: %s at %s" format
+        (latest_tweet_id, latest_tweet_time))
+      errprint("Highest tweet ID: %s" format highest_tweet_id)
+      errprint("Number of users: %s" format tweets_by_user.size)
+      output_reverse_sorted_table(tweets_by_user)
+    }
+  }
+
+  var curfile: String = _
+  var curfile_stats: TwitterStatistics = _
+  val global_stats = new TwitterStatistics
+
+  def begin_process_file(filehand: FileHandler, file: String,
+      compression: String, realname: String) {
+    val (_, outname) = filehand.split_filename(realname)
+    curfile = outname
+    curfile_stats = new TwitterStatistics
+  }
+
+  def process_line(metadata: Seq[String], text: String) {
+    curfile_stats.record_tweet(metadata, text)
+    global_stats.record_tweet(metadata, text)
+  }
+
+  def end_process_file(filehand: FileHandler, file: String) {
+    errprint("Statistics for file %s:" format curfile)
+    curfile_stats.print_statistics()
+  }
+
+  override def end_processing(filehand: FileHandler, files: Iterable[String]) {
+    errprint("Statistics for all files:")
+    global_stats.print_statistics()
+  }
+}
+
+class ConvertTwitterInfochimpsDriver extends ArgParserExperimentDriver {
+  type ParamType = ConvertTwitterInfochimpsParameters
+  type RunReturnType = Unit
+  
+  val filehand = new LocalFileHandler
+  
+  def usage() {
+    sys.error("""Usage: ConvertTwitterInfochimps [-o OUTDIR | --outfile OUTDIR] [--output-stats] INFILE ...
+
+Convert input files in the Infochimps Twitter corpus into files in the
+format expected by TextGrounder.  If --output-stats is given,
+output statistics to stderr rather than converting text.  Otherwise,
+store results in OUTDIR, which must not exist already.  
+""")
+  }
+
+  def handle_parameters() {
+    if (!params.output_stats)
+      need(params.output_dir, "output-dir")
+  }
+
+  def setup_for_run() { }
+
+  def run_after_setup() {
+    if (params.output_stats)
+      new TwitterInfochimpsStatsFileProcessor().
+        process_files(filehand, params.files)
+    else {
+      if (!filehand.make_directories(params.output_dir))
+        param_error("Output dir %s must not already exist" format
+          params.output_dir)
+      // First output the schema file
+      val fields = List("corpus", "title", "id", "group", "split", "coord",
+        "time", "username", "userid", "reply_username", "reply_userid",
+        "anchor", "lang", "text")
+      val schema_file_name =
+        "%s/twitter-infochimps-schema.txt" format params.output_dir
+      val schema_stream = filehand.openw(schema_file_name)
+      schema_stream.println(fields mkString "\t")
+      schema_stream.close()
+
+      new ConvertTwitterInfochimpsFileProcessor(params).
+        process_files(filehand, params.files)
+    }
   }
 }
 
