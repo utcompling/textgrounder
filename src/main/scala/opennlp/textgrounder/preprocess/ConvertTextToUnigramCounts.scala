1d983ace12126e1db4418a1bbb96e47103f689cd
Ben Wing <ben@benwing.com>
2011-11-27 12:25:02 -0600

    Major hacking of ConvertTextToUnigramCounts -- use ProcessCorpus.scala and current corpus format

--- src/main/scala/opennlp/textgrounder/preprocess/ConvertTextToUnigramCounts.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/ConvertTextToUnigramCounts.scala
@@ -16,169 +16,120 @@
 
 package opennlp.textgrounder.preprocess
 
-import java.io.InputStream
+import collection.mutable
 
-import com.nicta.scoobi._
-import com.nicta.scoobi.Scoobi._
-import com.nicta.scoobi.io.text._
-import com.nicta.scoobi.io.text.TextInput._
+// import com.nicta.scoobi._
+// import com.nicta.scoobi.Scoobi._
+// import com.nicta.scoobi.io.text._
+// import com.nicta.scoobi.io.text.TextInput._
 
 import opennlp.textgrounder.util.argparser._
 import opennlp.textgrounder.util.collectionutil._
-import opennlp.textgrounder.util.experiment._
-import opennlp.textgrounder.util.ioutil.{FileHandler, LocalFileHandler, TextFileProcessor}
-import opennlp.textgrounder.util.MeteredTask
-import opennlp.textgrounder.util.printutil.{errprint, output_reverse_sorted_table, set_errout_prefix}
+import opennlp.textgrounder.util.experiment.ExperimentDriverApp
+import opennlp.textgrounder.util.ioutil.FileHandler
+
+import opennlp.textgrounder.geolocate.GeoDocument
 
 /////////////////////////////////////////////////////////////////////////////
 //                                  Main code                              //
 /////////////////////////////////////////////////////////////////////////////
 
 class ConvertTextToUnigramCountsParameters(ap: ArgParser) extends
-    ArgParserParameters(ap) {
-  val output_dir =
-    ap.option[String]("o", "output-dir",
-      metavar = "DIR",
-      help = """Directory to store output files in.""")
-  val group_by_user =
-    ap.flag("group-by-user",
-      help = """If true, group tweets into a single per-user document.""")
-  val files =
-    ap.multiPositional[String]("files",
-      help = """File(s) to process for input.""")
+    ProcessCorpusParameters(ap) {
 }
 
 class ConvertTextToUnigramCountsFileProcessor(
+  schema: Seq[String], filehand: FileHandler, schema_file: String,
   params: ConvertTextToUnigramCountsParameters
-) extends TextFileProcessor {
-  def process_lines(lines: Iterator[String],
-      filehand: FileHandler, file: String,
-      compression: String, realname: String) = {
-    val compression_type = "bzip2"
-    val task = new MeteredTask("document", "processing")
-    val (_, basename) = filehand.split_filename(realname)
-    val out_basename = basename.replaceAll("-[^-]*$", "") + "-counts.txt"
-    val outname = filehand.join_filename(params.output_dir, out_basename)
-    errprint("Counts output file is %s..." format outname)
-    val outstream = filehand.openw(outname, compression = compression_type)
-
-    var lineno = 0
-    for (line <- lines) {
-      val counts = intmap[String]()
-      lineno += 1
-      line.split("\t", -1).toList match {
-        case title :: text :: Nil => {
-          outstream.println("Article title: %s" format title)
-          val words = text.split(' ')
-          for (word <- words)
-            counts(word) += 1
-          output_reverse_sorted_table(counts, outstream)
-        }
-        case _ => {
-          errprint("Bad line #%d: %s" format (lineno, line))
-          errprint("Line length: %d" format line.split("\t", -1).length)
-        }
-      }
-      task.item_processed()
-    }
-    outstream.close()
-    task.finish()
-    true
+) extends ProcessCorpusFileProcessor(
+  schema, filehand, schema_file, "-text", "-unigram-counts",
+  params.output_dir
+) {
+  def frob_row(fieldvals: Seq[String]) = {
+    val docparams = mutable.LinkedHashMap[String, String]()
+    docparams ++= (schema zip fieldvals)
+    val text = docparams("text")
+    docparams -= "text"
+    val counts = intmap[String]()
+    for (word <- text.split(" ", -1))
+      counts(word) += 1
+    val counts_text =
+      (for ((word, count) <- counts.toSeq sortWith (_._2 > _._2)) yield
+        ("%s:%s" format (GeoDocument.encode_word_for_counts_field(word),
+          count))) mkString " "
+    docparams += (("counts", counts_text))
+    docparams.toSeq
   }
-}
-
-abstract class BaseConvertTextToUnigramCountsDriver extends
-    ArgParserExperimentDriver {
-  type ParamType = ConvertTextToUnigramCountsParameters
-  type RunReturnType = Unit
-  
-  val filehand = new LocalFileHandler
-
-  val tweets_per_user = primmapmap[String, String, Int]()
-
-  def handle_parameters() {
-    need(params.output_dir, "output-dir")
-  }
-
-  def setup_for_run() { }
-
-  def run_after_setup() {
-    if (!filehand.make_directories(params.output_dir))
-      param_error("Output dir %s must not already exist" format
-        params.output_dir)
-
-    process_files(filehand, params.files)
-  }
-
-  def process_files(filehand: FileHandler, files: Seq[String])
 }
 
 class ConvertTextToUnigramCountsDriver extends
-    BaseConvertTextToUnigramCountsDriver {
+    ProcessCorpusDriver {
+  type ParamType = ConvertTextToUnigramCountsParameters
 
   def usage() {
-    sys.error("""Usage: ConvertTextToUnigramCounts [-o OUTDIR | --outfile OUTDIR] [--group-by-user] INFILE ...
+    sys.error("""Usage: ConvertTextToUnigramCounts [-u INDIR | --input-dir INDIR] [-o OUTDIR | --output-dir OUTDIR]
 
-Convert input files from raw-text format (one document per line) into unigram
-counts, in the format expected by TextGrounder.  OUTDIR is the directory to
-store the results in, which must not exist already.  If --group-by-user is
-given, a document is the concatenation of all tweets for a given user.
-Else, each individual tweet is a document.
+Convert a corpus from raw-text format to unigram-counts format.  OUTDIR is
+the directory to store the output corpus in, which must not exist already.
+INDIR is the directory of the input corpus.
 """)
   }
 
-  def process_files(filehand: FileHandler, files: Seq[String]) {
-    new ConvertTextToUnigramCountsFileProcessor(params).
-      process_files(filehand, files)
-  }
+  def create_file_processor(schema: Seq[String], filehand: FileHandler,
+      schema_file: String) =
+    new ConvertTextToUnigramCountsFileProcessor(schema, filehand, schema_file,
+      params)
+
+  def get_input_corpus_suffix = "-text"
 }
 
-class ScoobiConvertTextToUnigramCountsDriver extends
-    BaseConvertTextToUnigramCountsDriver {
+//class ScoobiConvertTextToUnigramCountsDriver extends
+//    BaseConvertTextToUnigramCountsDriver {
+//
+//  def usage() {
+//    sys.error("""Usage: ScoobiConvertTextToUnigramCounts [-o OUTDIR | --outfile OUTDIR] [--group-by-user] INFILE ...
+//
+//Using Scoobi (front end to Hadoop), convert input files from raw-text format
+//(one document per line) into unigram counts, in the format expected by
+//TextGrounder.  OUTDIR is the directory to store the results in, which must not
+//exist already.  If --group-by-user is given, a document is the concatenation
+//of all tweets for a given user.  Else, each individual tweet is a document.
+//""")
+//  }
+//
+//  def process_files(filehand: FileHandler, files: Seq[String]) {
+//    val task = new MeteredTask("tweet", "processing")
+//    var tweet_lineno = 0
+//    val task2 = new MeteredTask("user", "processing")
+//    var user_lineno = 0
+//    val out_counts_name = "%s/counts-only-coord-documents.txt" format (params.output_dir)
+//    errprint("Counts output file is %s..." format out_counts_name)
+//    val tweets = extractFromDelimitedTextFile("\t", params.files(0)) {
+//      case user :: title :: text :: Nil => (user, text)
+//    }
+//    val counts = tweets.groupByKey.map {
+//      case (user, tweets) => {
+//        val counts = intmap[String]()
+//        tweet_lineno += 1
+//        for (tweet <- tweets) {
+//          val words = tweet.split(' ')
+//          for (word <- words)
+//            counts(word) += 1
+//        }
+//        val result =
+//          (for ((word, count) <- counts) yield "%s:%s" format (word, count)).
+//            mkString(" ")
+//        (user, result)
+//      }
+//    }
+//
+//    // Execute everything, and throw it into a directory
+//    DList.persist (
+//      TextOutput.toTextFile(counts, out_counts_name)
+//    )
+//  }
+//}
 
-  def usage() {
-    sys.error("""Usage: ScoobiConvertTextToUnigramCounts [-o OUTDIR | --outfile OUTDIR] [--group-by-user] INFILE ...
-
-Using Scoobi (front end to Hadoop), convert input files from raw-text format
-(one document per line) into unigram counts, in the format expected by
-TextGrounder.  OUTDIR is the directory to store the results in, which must not
-exist already.  If --group-by-user is given, a document is the concatenation
-of all tweets for a given user.  Else, each individual tweet is a document.
-""")
-  }
-
-  def process_files(filehand: FileHandler, files: Seq[String]) {
-    val task = new MeteredTask("tweet", "processing")
-    var tweet_lineno = 0
-    val task2 = new MeteredTask("user", "processing")
-    var user_lineno = 0
-    val out_counts_name = "%s/counts-only-coord-documents.txt" format (params.output_dir)
-    errprint("Counts output file is %s..." format out_counts_name)
-    val tweets = extractFromDelimitedTextFile("\t", params.files(0)) {
-      case user :: title :: text :: Nil => (user, text)
-    }
-    val counts = tweets.groupByKey.map {
-      case (user, tweets) => {
-        val counts = intmap[String]()
-        tweet_lineno += 1
-        for (tweet <- tweets) {
-          val words = tweet.split(' ')
-          for (word <- words)
-            counts(word) += 1
-        }
-        val result =
-          (for ((word, count) <- counts) yield "%s:%s" format (word, count)).
-            mkString(" ")
-        (user, result)
-      }
-    }
-
-    // Execute everything, and throw it into a directory
-    DList.persist (
-      TextOutput.toTextFile(counts, out_counts_name)
-    )
-  }
-}
 object ConvertTextToUnigramCounts extends
     ExperimentDriverApp("Convert raw text to unigram counts") {
   type DriverType = ConvertTextToUnigramCountsDriver
@@ -186,25 +137,25 @@ object ConvertTextToUnigramCounts extends
   def create_driver() = new DriverType
 }
 
-abstract class ScoobiApp(
-  progname: String
-) extends ExperimentDriverApp(progname) {
-  override def main(orig_args: Array[String]) = withHadoopArgs(orig_args) {
-    args => {
-      /* Thread execution back to the ExperimentDriverApp.  This will read
-         command-line arguments, call initialize_parameters() to verify
-         and canonicalize them, and then pass control back to us by
-         calling run_program(), which we override. */
-      set_errout_prefix(progname + ": ")
-      implement_main(args)
-    }
-  }
-}
-
-object ScoobiConvertTextToUnigramCounts extends
-    ScoobiApp("Convert raw text to unigram counts") {
-  type DriverType = ScoobiConvertTextToUnigramCountsDriver
-  def create_param_object(ap: ArgParser) = new ParamType(ap)
-  def create_driver() = new DriverType
-}
+//abstract class ScoobiApp(
+//  progname: String
+//) extends ExperimentDriverApp(progname) {
+//  override def main(orig_args: Array[String]) = withHadoopArgs(orig_args) {
+//    args => {
+//      /* Thread execution back to the ExperimentDriverApp.  This will read
+//         command-line arguments, call initialize_parameters() to verify
+//         and canonicalize them, and then pass control back to us by
+//         calling run_program(), which we override. */
+//      set_errout_prefix(progname + ": ")
+//      implement_main(args)
+//    }
+//  }
+//}
+//
+//object ScoobiConvertTextToUnigramCounts extends
+//    ScoobiApp("Convert raw text to unigram counts") {
+//  type DriverType = ScoobiConvertTextToUnigramCountsDriver
+//  def create_param_object(ap: ArgParser) = new ParamType(ap)
+//  def create_driver() = new DriverType
+//}
 
