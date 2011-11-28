5549ad525fd07f60903588cfa341de93e4615adf
Ben Wing <ben@benwing.com>
2011-11-28 05:28:31 -0600

    Lots and lots more statistics computed in ConvertTwitterInfochimps (probably some will end up turned off by default)

--- src/main/scala/opennlp/textgrounder/preprocess/ConvertTwitterInfochimps.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/ConvertTwitterInfochimps.scala
@@ -16,7 +16,9 @@
 
 package opennlp.textgrounder.preprocess
 
-import java.io.{InputStream, PrintStream}
+import collection.mutable
+
+import java.io.PrintStream
 
 import org.apache.commons.lang3.StringEscapeUtils._
 
@@ -25,7 +27,9 @@ import opennlp.textgrounder.util.collectionutil._
 import opennlp.textgrounder.util.experiment._
 import opennlp.textgrounder.util.ioutil.{FileHandler, LocalFileHandler, TextFileProcessor}
 import opennlp.textgrounder.util.MeteredTask
-import opennlp.textgrounder.util.printutil.{errprint, output_reverse_sorted_table, set_errout_prefix}
+import opennlp.textgrounder.util.osutil.output_resource_usage
+import opennlp.textgrounder.util.printutil._
+import opennlp.textgrounder.util.textutil.long_with_commas
 import opennlp.textgrounder.util.Twokenize
 
 /*
@@ -114,6 +118,7 @@ abstract class TwitterInfochimpsFileProcessor extends TextFileProcessor {
       task.item_processed()
     }
     task.finish()
+    output_resource_usage()
     true
   }
 
@@ -168,100 +173,263 @@ class ConvertTwitterInfochimpsFileProcessor(
   }
 }
 
-class TwitterInfochimpsStatsFileProcessor extends
-    TwitterInfochimpsFileProcessor {
-  class TwitterStatistics {
-    // Over-all statistics
-    var num_tweets = 0
+class TwitterStatistics {
+  // Over-all statistics
+  var num_tweets = 0
 
-    // Time-based statistics
-    val tweets_by_day = intmap[String]()
-    val tweets_by_hour = intmap[String]()
-    val tweets_by_minute = intmap[String]()
-    // In general, when finding the minimum, we want the default to be greater
-    // than any possible value, and when finding the maximum, we want the
-    // default to be less than any possible value.  For string comparison,
-    // the empty string is less than any other string, and a string beginning
-    // with Unicode 0xFFFF is greater than almost any other string. (0xFFFF
-    // is not a valid Unicode character.)
-    var earliest_tweet_time = "\uFFFF"
-    var latest_tweet_time = ""
-    var earliest_tweet_id = "\uFFFF"
-    var latest_tweet_id = ""
+  val max_string_val = "\uFFFF"
+  val min_string_val = ""
+  // Time-based statistics
+  val tweets_by_day = intmap[String]()
+  val tweets_by_hour = intmap[String]()
+  val tweets_by_minute = intmap[String]()
+  // In general, when finding the minimum, we want the default to be greater
+  // than any possible value, and when finding the maximum, we want the
+  // default to be less than any possible value.  For string comparison,
+  // the empty string is less than any other string, and a string beginning
+  // with Unicode 0xFFFF is greater than almost any other string. (0xFFFF
+  // is not a valid Unicode character.)
+  var earliest_tweet_time = max_string_val
+  var latest_tweet_time = min_string_val
+  var earliest_tweet_id = max_string_val
+  var latest_tweet_id = min_string_val
 
-    // If tweet ID's are sorted by time, the following two should be
-    // identical to the earliest_tweet_id/latest_tweet_id.
-    var lowest_tweet_id = "\uFFFF"
-    var highest_tweet_id = ""
+  // If tweet ID's are sorted by time, the following two should be
+  // identical to the earliest_tweet_id/latest_tweet_id.  Tweets ID's
+  // may have exceeeded Long range; if not, they probably will soon.
+  var num_lowest_tweet_id = BigInt("9"*50)
+  var num_lowest_tweet_time = ""
+  var num_highest_tweet_id = BigInt(-1)
+  var num_highest_tweet_time = ""
 
-    // User-based statistics
-    val tweets_by_user = intmap[String]()
-    val userid_by_user = setmap[String, String]()
-    val reply_userid_by_reply_user = setmap[String, String]()
-    val reply_user_by_user = setmap[String, String]()
+  // Similar but we sort tweet ID's lexicographically.
+  var lex_lowest_tweet_id = max_string_val
+  var lex_lowest_tweet_time = ""
+  var lex_highest_tweet_id = ""
+  var lex_highest_tweet_time = ""
 
-    def record_tweet(metadata: Seq[(String, String)], text: String) {
-      val params = metadata.toMap
-      val time = params("time")
-      val id = params("id")
-      val username = params("username")
-      val userid = params("userid")
-      val reply_username = params("reply_username")
-      val reply_userid = params("reply_userid")
+  // User-based statistics
+  val tweets_by_user = intmap[String]()
+  val reply_tweets_by_user = intmap[String]()
+  val num_users_by_num_tweets = intmap[Int]()
+  val userid_by_user = intmapmap[String, String]()
+  val userid_by_user_min_time = stringmapmap[String, String](max_string_val)
+  val userid_by_user_max_time = stringmapmap[String, String](min_string_val)
+  val user_by_userid = intmapmap[String, String]()
+  val user_by_userid_min_time = stringmapmap[String, String](max_string_val)
+  val user_by_userid_max_time = stringmapmap[String, String](min_string_val)
+  val tweets_by_reply_user = intmap[String]()
+  val reply_userid_by_reply_user = intmapmap[String, String]()
+  val reply_userid_by_reply_user_min_time = stringmapmap[String, String](max_string_val)
+  val reply_userid_by_reply_user_max_time = stringmapmap[String, String](min_string_val)
+  val reply_user_by_reply_userid = intmapmap[String, String]()
+  val reply_user_by_reply_userid_min_time = stringmapmap[String, String](max_string_val)
+  val reply_user_by_reply_userid_max_time = stringmapmap[String, String](min_string_val)
+  val reply_user_by_user = intmapmap[String, String]()
+  val user_by_reply_user = intmapmap[String, String]()
 
-      num_tweets += 1
+  def record_tweet(metadata: Seq[(String, String)], text: String) {
+    val params = metadata.toMap
+    val time = params("time")
+    val id = params("id")
+    val username = params("username")
+    val userid = params("userid")
+    val reply_username = params("reply_username")
+    val reply_userid = params("reply_userid")
 
-      // Time format is YYYYMMDDHHmmSS, hence take(8) goes up through the day,
-      // take(10) up through the hour, etc.
-      tweets_by_day(time.take(8)) += 1
-      tweets_by_hour(time.take(10)) += 1
-      tweets_by_minute(time.take(12)) += 1
+    num_tweets += 1
 
-      if (time < earliest_tweet_time) {
-        earliest_tweet_time = time
-        earliest_tweet_id = id
-      }
-      if (time > latest_tweet_time) {
-        latest_tweet_time = time
-        latest_tweet_id = id
-      }
-      if (id < lowest_tweet_id)
-        lowest_tweet_id = id
-      if (id > highest_tweet_id)
-        highest_tweet_id = id
+    // Time format is YYYYMMDDHHmmSS, hence take(8) goes up through the day,
+    // take(10) up through the hour, etc.
+    tweets_by_day(time.take(8)) += 1
+    tweets_by_hour(time.take(10)) += 1
+    // tweets_by_minute(time.take(12)) += 1
 
-      tweets_by_user(username) += 1
-      userid_by_user(username) += userid
-      if (reply_username != "") {
-        reply_userid_by_reply_user(reply_username) += reply_userid
-        reply_user_by_user(username) += reply_username
-      }
+    if (time < earliest_tweet_time) {
+      earliest_tweet_time = time
+      earliest_tweet_id = id
+    }
+    if (time > latest_tweet_time) {
+      latest_tweet_time = time
+      latest_tweet_id = id
+    }
+    if (id < lex_lowest_tweet_id) {
+      lex_lowest_tweet_id = id
+      lex_lowest_tweet_time = time
+    }
+    if (id > lex_highest_tweet_id) {
+      lex_highest_tweet_id = id
+      lex_highest_tweet_time = time
+    }
+    val num_id = BigInt(id)
+    if (num_id < num_lowest_tweet_id) {
+      num_lowest_tweet_id = num_id
+      num_lowest_tweet_time = time
+    }
+    if (num_id > num_highest_tweet_id) {
+      num_highest_tweet_id = num_id
+      num_highest_tweet_time = time
     }
 
-    def print_statistics() {
-      errprint("Number of tweets: %s" format num_tweets)
-      errprint("Earliest tweet: %s at %s" format
-        (earliest_tweet_id, earliest_tweet_time))
-      errprint("Lowest tweet ID: %s" format lowest_tweet_id)
-      errprint("Latest tweet: %s at %s" format
-        (latest_tweet_id, latest_tweet_time))
-      errprint("Highest tweet ID: %s" format highest_tweet_id)
-      errprint("Number of users: %s" format tweets_by_user.size)
-      output_reverse_sorted_table(tweets_by_user)
+
+    def set_max_with_cur[T,U <% Ordered[U]](table: mutable.Map[T,U],
+        key: T, newval: U) {
+      if (table(key) < newval)
+        table(key) = newval
+    }
+    def set_min_with_cur[T,U <% Ordered[U]](table: mutable.Map[T,U],
+        key: T, newval: U) {
+      if (table(key) > newval)
+        table(key) = newval
+    }
+    tweets_by_user(username) += 1
+    userid_by_user(username)(userid) += 1
+    set_max_with_cur(userid_by_user_max_time(username), userid, time)
+    set_min_with_cur(userid_by_user_min_time(username), userid, time)
+    user_by_userid(userid)(username) += 1
+    set_max_with_cur(user_by_userid_max_time(userid), username, time)
+    set_min_with_cur(user_by_userid_min_time(userid), username, time)
+    if (reply_username != "") {
+      reply_tweets_by_user(username) += 1
+      tweets_by_reply_user(reply_username) += 1
+      reply_userid_by_reply_user(reply_username)(reply_userid) += 1
+      reply_user_by_reply_userid(reply_userid)(reply_username) += 1
+      set_max_with_cur(reply_userid_by_reply_user_max_time(reply_username), reply_userid, time)
+      set_min_with_cur(reply_userid_by_reply_user_min_time(reply_username), reply_userid, time)
+      set_max_with_cur(reply_user_by_reply_userid_max_time(reply_userid), reply_username, time)
+      set_min_with_cur(reply_user_by_reply_userid_min_time(reply_userid), reply_username, time)
+      reply_user_by_user(username)(reply_username) += 1
+      user_by_reply_user(reply_username)(username) += 1
     }
   }
 
+  def finish_statistics() {
+    num_users_by_num_tweets.clear()
+    for ((user, count) <- tweets_by_user)
+      num_users_by_num_tweets(count) += 1
+  }
+
+  def print_statistics() {
+    finish_statistics()
+
+    val how_many_summary = 10000
+    val how_many_summary_str = long_with_commas(how_many_summary)
+    val how_many_detail = 100
+    val how_many_detail_str = long_with_commas(how_many_detail)
+
+    errprint("")
+    errprint("Earliest tweet: %s at %s" format
+      (earliest_tweet_id, earliest_tweet_time))
+    errprint("Numerically lowest tweet ID: %s at %s" format
+      (num_lowest_tweet_id, num_lowest_tweet_time))
+    errprint("Lexicographically lowest tweet ID: %s at %s" format
+      (lex_lowest_tweet_id, lex_lowest_tweet_time))
+    errprint("")
+    errprint("Latest tweet: %s at %s" format
+      (latest_tweet_id, latest_tweet_time))
+    errprint("Numerically highest tweet ID: %s at %s" format
+      (num_highest_tweet_id, num_highest_tweet_time))
+    errprint("Lexicographically highest tweet ID: %s at %s" format
+      (lex_highest_tweet_id, lex_highest_tweet_time))
+    errprint("")
+    errprint("Number of tweets: %s" format num_tweets)
+    errprint("Number of users: %s" format tweets_by_user.size)
+
+    print_msg_heading(
+      "Top %s users by number of tweets:" format how_many_summary_str)
+    output_reverse_sorted_table(tweets_by_user, maxrows = how_many_summary)
+
+    print_msg_heading(
+      "Frequency of frequencies (number of users with given number of tweets):")
+    output_key_sorted_table(num_users_by_num_tweets)
+
+
+    def reply_to_details(sending: String, _from: String, _to: String,
+        tweets_by_user_map: mutable.Map[String, Int],
+        tweets_by_reply_user_map: mutable.Map[String, Int]) {
+      print_msg_heading("Reply-to, for top %s %s users:" format
+        (how_many_detail_str, sending))
+      for (((user, count), index0) <-
+          tweets_by_user_map.toSeq.sortWith(_._2 > _._2).
+          slice(0, how_many_detail).
+          zipWithIndex) {
+        val index = index0 + 1
+        errprint("#%d: User %s (%d tweets %s, %d tweets %s):",
+          index, user, count, _from, tweets_by_reply_user_map(user), _to)
+        errprint("#%d: Corresponding user ID's by tweet count:" format (index))
+        output_reverse_sorted_table(userid_by_user(user), indent = "   ")
+        errprint("#%d: Users that this user replied to:" format (index))
+        output_reverse_sorted_table(reply_user_by_user(user), indent = "   ")
+        errprint("#%d: Users that relied to this user:" format (index))
+        output_reverse_sorted_table(user_by_reply_user(user), indent = "   ")
+      }
+    }
+
+    reply_to_details("sending", "from", "to", tweets_by_user,
+      tweets_by_reply_user)
+    reply_to_details("receiving", "to", "from", tweets_by_reply_user,
+      tweets_by_user)
+
+    def output_x_with_multi_y(xdesc: String, ydesc: String,
+        x_to_y: mutable.Map[String, mutable.Map[String, Int]],
+        x_to_y_min_time: mutable.Map[String, mutable.Map[String, String]],
+        x_to_y_max_time: mutable.Map[String, mutable.Map[String, String]]
+      ) {
+      val x_with_multi_y =
+        (for ((x, ys) <- x_to_y; if ys.size > 1)
+          yield (x, ys.size))
+      for (((x, count), index) <-
+           x_with_multi_y.toSeq.sortWith(_._2 > _._2).zipWithIndex) {
+        errprint("#%d: %s %s (%d different %s's): (listed by num tweets)",
+          index + 1, xdesc, x, count, ydesc)
+        for ((y, count) <- x_to_y(x).toSeq.sortWith(_._2 > _._2)) {
+          errprint("%s%s = %s (from %s to %s)" format
+            ("   ", y, count, x_to_y_min_time(x)(y), x_to_y_max_time(x)(y)))
+        }
+      }
+    }
+
+    print_msg_heading("Users with multiple user ID's:")
+    output_x_with_multi_y("user", "ID", userid_by_user,
+      userid_by_user_min_time, userid_by_user_max_time)
+
+    print_msg_heading("User ID's with multiple users:")
+    output_x_with_multi_y("ID", "user", user_by_userid,
+      user_by_userid_min_time, user_by_userid_max_time)
+
+    print_msg_heading("Reply users with multiple reply user ID's:")
+    output_x_with_multi_y("reply user", "reply ID", reply_userid_by_reply_user,
+      reply_userid_by_reply_user_min_time, reply_userid_by_reply_user_max_time)
+
+    print_msg_heading("Reply ID's with multiple reply users:")
+    output_x_with_multi_y("reply ID", "reply user", reply_user_by_reply_userid,
+      reply_user_by_reply_userid_min_time, reply_user_by_reply_userid_max_time)
+
+    print_msg_heading("Tweets by day:")
+    output_key_sorted_table(tweets_by_day)
+    print_msg_heading("Tweets by hour:")
+    output_key_sorted_table(tweets_by_hour)
+    if (tweets_by_minute.size > 0) {
+      print_msg_heading("Tweets by minute:")
+      output_key_sorted_table(tweets_by_minute)
+    }
+
+    print_msg_heading("Memory/time usage:", blank_lines_before = 3)
+    output_resource_usage()
+  }
+}
+
+class TwitterInfochimpsStatsFileProcessor extends
+    TwitterInfochimpsFileProcessor {
   var curfile: String = _
   var curfile_stats: TwitterStatistics = _
   val global_stats = new TwitterStatistics
 
-  override def begin_process_lines(lines: Iterator[String],
-      filehand: FileHandler, file: String,
-      compression: String, realname: String) {
-    val (_, outname) = filehand.split_filename(realname)
+  override def begin_process_file(filehand: FileHandler, file: String) {
+    val (_, outname) = filehand.split_filename(file)
     curfile = outname
     curfile_stats = new TwitterStatistics
-    super.begin_process_lines(lines, filehand, file, compression, realname)
+    super.begin_process_file(filehand, file)
   }
 
   def process_line(metadata: Seq[(String, String)], text: String) {
@@ -269,15 +437,28 @@ class TwitterInfochimpsStatsFileProcessor extends
     global_stats.record_tweet(metadata, text)
   }
 
-  override def end_process_file(filehand: FileHandler, file: String) {
-    errprint("Statistics for file %s:" format curfile)
+  def print_curfile_stats() {
+    print_msg_heading("Statistics for file %s:" format curfile,
+      blank_lines_before = 6)
     curfile_stats.print_statistics()
+  }
+
+  def print_global_stats(is_final: Boolean = false) {
+    print_msg_heading(
+      "Statistics for all files%s:" format (if (is_final) "" else " so far"),
+      blank_lines_before = 6)
+    global_stats.print_statistics()
+  }
+
+  override def end_process_file(filehand: FileHandler, file: String) {
+    print_curfile_stats()
+    print_global_stats()
     super.end_process_file(filehand, file)
   }
 
   override def end_processing(filehand: FileHandler, files: Iterable[String]) {
-    errprint("Statistics for all files:")
-    global_stats.print_statistics()
+    print_global_stats()
+    super.end_processing(filehand, files)
   }
 }
 
