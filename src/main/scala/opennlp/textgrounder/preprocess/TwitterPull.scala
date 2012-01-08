653493e018786ca6a435613cfb1f1f040c45fa20
Stephen Roller <roller@cs.utexas.edu>
2012-01-08 17:33:17 -0600

    Downgrade scoobi (blah) and update to TwitterPull that kind of works, but dies when strings get too long due to a Scoobi/Java nuance.

--- src/main/scala/opennlp/textgrounder/preprocess/TwitterPull.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/TwitterPull.scala
@@ -5,32 +5,108 @@ import com.nicta.scoobi._
 import com.nicta.scoobi.Scoobi._
 import com.nicta.scoobi.io.text._
 import java.io._
+import java.lang.Double.isNaN
 
 object TwitterPull {
+  type Tweet = (Long, String, Double, Double)
+  type Record = (String, Tweet)
+  type Author = (String, Long, Double, Double)
+  type AuthorWord = (Author, String)
+  type WordCount = (String, Long)
 
   def force_value(value: json.JValue) : String = {
-    (value values) toString
+    if ((value values) == null)
+        null
+    else
+        (value values) toString
   }
 
-  def parse_json(line: String): (String, String, String, Double, Double) = {
+  def parse_time(timestring: String): Long = {
+    val sdf = new java.text.SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy")
+    sdf.parse(timestring)
+    sdf.getCalendar.getTimeInMillis
+  }
+
+  def is_valid_tweet(line: String): Boolean = {
+    !line.contains("""{"limit":{"track":""")
+  }
+
+  def parse_json(line: String): Record = {
     val parsed = json.parse(line)
     val author = force_value(parsed \ "user" \ "screen_name")
-    val timestamp = force_value(parsed \ "created_at")
+    val timestamp = parse_time(force_value(parsed \ "created_at"))
     val text = force_value(parsed \ "text")
     val (lat, lng) = 
-      if ((parsed \ "coordinate" values) == null ||
-          (force_value(parsed \ "coordinate" \ "type") != "Point")) {
+      if ((parsed \ "coordinates" values) == null ||
+          (force_value(parsed \ "coordinates" \ "type") != "Point")) {
         (Double.NaN, Double.NaN)
       } else {
-        val latlng: List[Double] = 
-          (parsed \ "coordinate" \ "coordinate" values).asInstanceOf[List[Double]]
-        (latlng(0), latlng(1))
+        val latlng: List[Number] = 
+          (parsed \ "coordinates" \ "coordinates" values).asInstanceOf[List[Number]]
+        (latlng(0).doubleValue, latlng(1).doubleValue)
       }
-    (author, timestamp, text, lat, lng)
+    (author, (timestamp, text, lat, lng))
+  }
+
+  def merge_records(tweet1: Tweet, tweet2: Tweet): Tweet = {
+    val (ts1, text1, lat1, lng1) = tweet1
+    val (ts2, text2, lat2, lng2) = tweet2
+    val text = text1 + " " + text2
+
+    val (lat, lng, ts) = 
+      if (isNaN(lat1) && isNaN(lat2)) {
+        (lat1, lng1, math.min(ts1, ts2))
+      } else if (isNaN(lat2)) {
+        (lat1, lng1, ts1)
+      } else if (isNaN(lat1)) {
+        (lat2, lng2, ts2)
+      } else if (ts1 < ts2) {
+        (lat1, lng1, ts1)
+      } else {
+        (lat2, lng2, ts2)
+      }
+
+    (ts, text, lat, lng)
+  }
+
+  def has_latlng(r: Record): Boolean = {
+    val (a, (ts, text, lat, lng)) = r
+    !isNaN(lat) && !isNaN(lng)
+  }
+
+  def tokenize(text: String): Iterable[String] = {
+    return text.replaceAll("\\s+", " ").split(" ")
+  }
+
+  val MAX_WORD_SIZE = 32 * 1024 - 1
+
+  def emit_words(r: Record): Iterable[(AuthorWord, Long)] = {
+    val (author, (ts, text, lat, lng)) = r
+    for (word <- tokenize(text) if word.length < MAX_WORD_SIZE)
+      yield (((author, ts, lat, lng), word), 1L)
+  }
+
+  def reposition_word(awc: (AuthorWord, Long)): (Author, WordCount) = {
+    val ((author, word), c) = awc
+    (author, (word, c))
+  }
+
+  /*
+  def record_to_string(awcs: (Author, Iterable[WordCount])): String = {
+    val ((author, ts, lat, lng), wcs) = awcs
+    val nice_text = wcs.map((w: WordCount) => w._1 + ":" + w._2).mkString(" ")
+    author + "\t" + ts + "\t" + lat + "," + lng + "\t" + nice_text
+  }
+  */
+
+  def record_to_string(awcs: (AuthorWord, Long)): String = {
+    val (((author, ts, lat, lng), word), count) = awcs
+    author + "\t" + ts + "\t" + lat + "," + lng + "\t" + word + "\t" + count
   }
 
   def main(args: Array[String]) = withHadoopArgs(args) { a =>
 
+    // make sure we get all the input
     val (inputPath, outputPath) =
       if (a.length == 2) {
         (a(0), a(1))
@@ -38,27 +114,31 @@ object TwitterPull {
         sys.error("Expecting input and output path.")
       }
 
-    // Firstly we load up all the (new-line-seperated) words into a DList
+    // Firstly we load up all the (new-line-seperated) json lines
     val lines: DList[String] = TextInput.fromTextFile(inputPath)
 
-    val values_extracted = lines.map(parse_json)
-    DList.persist(TextOutput.toTextFile(values_extracted, outputPath))
+    // Filter out some trivially invalid tweets and parse the json
+    val values_extracted = lines.filter(is_valid_tweet).map(parse_json)
 
-    /*
-    // What we want to do, is record the frequency of words. So we'll convert it to a key-value
-    // pairs where the key is the word, and the value the frequency (which to start with is 1)
-    val keyValuePair: DList[(String, Int)] = lines flatMap { _.split(" ") } map { w => (w, 1) }
+    // group by author, combine the records, keeping the earliest
+    // tweet with a specific coordinate
+    val concatted = values_extracted.groupByKey.combine(merge_records)
 
-    // Now let's group all words that compare the same
-    val grouped: DList[(String, Iterable[Int])] = keyValuePair.groupByKey
-    // Now we have it in the form (Word, ['1', '1', '1', 1' etc.])
+    // filter every user that doesn't have a specific coordinate
+    val with_coord = concatted.filter(has_latlng)
 
-    // So what we want to do, is combine all the numbers into a single value (the frequency)
-    val combined: DList[(String, Int)] = grouped.combine((_+_))
+    // word count
+    val emitted_words = with_coord.flatMap(emit_words)
+    val word_counts = emitted_words.groupByKey.combine((a: Long, b: Long) => (a + b))
 
-    // We can evalute this, and write it to a text file
-    DList.persist(TextOutput.toTextFile(combined, outputPath + "/word-results"));
-    */
+    // regroup with author as key, word pairs as values
+    //val regrouped_by_author = word_counts.map(reposition_word).groupByKey
+
+    val nicely_formatted = word_counts.map(record_to_string)
+
+    // save to disk
+    DList.persist(TextOutput.toTextFile(nicely_formatted, outputPath))
+
   }
-
 }
+
