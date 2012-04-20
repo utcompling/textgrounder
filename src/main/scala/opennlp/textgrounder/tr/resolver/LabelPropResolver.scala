b2ea26df10c549737df0effa5e751d57a31fc8b6
Mike Speriosu <mike@subtextsolutions.com>
2012-04-20 01:12:00 -0500

    Added command line option for labelprop resolver, added stub LabelPropResolver.

---
+++ src/main/scala/opennlp/textgrounder/tr/resolver/LabelPropResolver.scala
@@ -0,0 +1,16 @@
+package opennlp.textgrounder.tr.resolver
+
+import opennlp.textgrounder.tr.text._
+import opennlp.textgrounder.tr.topo._
+
+import upenn.junto.app._
+import upenn.junto.config._
+
+class LabelPropResolver extends Resolver {
+
+  def disambiguate(corpus:StoredCorpus): StoredCorpus = {
+    
+    null
+  }
+
+}
