7b4dd7d00429cd397838b81fe10b5651bd1b2b72
Mike Speriosu <mike@subtextsolutions.com>
2012-04-27 11:43:18 -0500

    Fixed bug in TopoUtil.getCellNumber. Added first version of LogFileParseElement.getProbDistOverPredCells.

--- src/main/scala/opennlp/textgrounder/tr/util/LogUtil.scala
+++ src/main/scala/opennlp/textgrounder/tr/util/LogUtil.scala
@@ -5,6 +5,9 @@ import opennlp.textgrounder.tr.topo._
 object LogUtil {
 
   val DOC_PREFIX = "Document "
+  val PRED_CELL_RANK_PREFIX = "  Predicted cell (at rank "
+  val PRED_CELL_KL_PREFIX = ", kl-div "
+  val PRED_CELL_BOTTOM_LEFT_COORD_PREFIX = "): GeoCell(("
   val TRUE_COORD_PREFIX = " at ("
   val PRED_COORD_PREFIX = " predicted cell center at ("
   val NEIGHBOR_PREFIX = " close neighbor: ("
@@ -14,6 +17,7 @@ object LogUtil {
 
     var docName:String = null
     var neighbors:List[(Coordinate, Int)] = null
+    var predCells:List[(Int, Double, Coordinate)] = null
     var trueCoord:Coordinate = null
     var predCoord:Coordinate = null
 
@@ -31,7 +35,25 @@ object LogUtil {
           val rawCoords = line.slice(startIndex, endIndex).split(",")
           trueCoord = Coordinate.fromDegrees(rawCoords(0).toDouble, rawCoords(1).toDouble)
 
+          predCells = List()
           neighbors = List()
+
+          None
+        }
+
+        else if(line.contains(PRED_CELL_RANK_PREFIX)) {
+          val rankStartIndex = line.indexOf(PRED_CELL_RANK_PREFIX) + PRED_CELL_RANK_PREFIX.length
+          val rankEndIndex = line.indexOf(PRED_CELL_KL_PREFIX, rankStartIndex)
+          val rank = line.slice(rankStartIndex, rankEndIndex).toInt
+          val klStartIndex = rankEndIndex + PRED_CELL_KL_PREFIX.length
+          val klEndIndex = line.indexOf(PRED_CELL_BOTTOM_LEFT_COORD_PREFIX, klStartIndex)
+          val kl = line.slice(klStartIndex, klEndIndex).toDouble
+          val blCoordStartIndex = klEndIndex + PRED_CELL_BOTTOM_LEFT_COORD_PREFIX.length
+          val blCoordEndIndex = line.indexOf(")", blCoordStartIndex)
+          val rawBlCoord = line.slice(blCoordStartIndex, blCoordEndIndex).split(",")
+          val blCoord = Coordinate.fromDegrees(rawBlCoord(0).toDouble, rawBlCoord(1).toDouble)
+
+          predCells = predCells ::: ((rank, kl, blCoord) :: Nil)
 
           None
         }
@@ -56,9 +78,7 @@ object LogUtil {
           val rawCoords = line.slice(startIndex, endIndex).split(",")
           predCoord = Coordinate.fromDegrees(rawCoords(0).toDouble, rawCoords(1).toDouble)
 
-          Some(new LogFileParseElement(docName, trueCoord, predCoord, neighbors))
-
-          //Some((docName, trueCoord, predCoord, neighbors))
+          Some(new LogFileParseElement(docName, trueCoord, predCoord, predCells, neighbors))
         }
 
         else None
@@ -73,5 +93,15 @@ class LogFileParseElement(
   val docName: String,
   val trueCoord: Coordinate,
   val predCoord: Coordinate,
+  val predCells: List[(Int, Double, Coordinate)],
   val neighbors: List[(Coordinate, Int)]) {
+
+    def getProbDistOverPredCells(dpc: Double): Map[Int, Double] = {
+      var sum = 0.0
+      (for((rank, kl, blCoord) <- predCells) yield {
+        val unnormalized = math.exp(-kl)
+        sum += unnormalized
+        (TopoUtil.getCellNumber(blCoord, dpc), unnormalized)
+      }).map(p => (p._1, p._2/sum)).toMap
+    }
 }
