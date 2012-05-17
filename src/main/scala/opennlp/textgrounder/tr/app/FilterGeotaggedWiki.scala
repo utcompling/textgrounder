b2361bb765bdba6e1b616862c42c6f71d64f603d
Mike Speriosu <mike@subtextsolutions.com>
2012-05-16 20:51:43 -0500

    Added tool for filtering out non-geotagged Wikipedia articles from a tokenized corpus.

---
+++ src/main/scala/opennlp/textgrounder/tr/app/FilterGeotaggedWiki.scala
@@ -0,0 +1,67 @@
+package opennlp.textgrounder.tr.app
+
+import java.io._
+import java.util.zip._
+
+import opennlp.textgrounder.tr.util._
+import opennlp.textgrounder.tr.topo._
+import opennlp.textgrounder.tr.topo.gaz._
+import opennlp.textgrounder.tr.text._
+import opennlp.textgrounder.tr.text.prep._
+import opennlp.textgrounder.tr.text.io._
+
+import scala.collection.JavaConversions._
+
+import org.apache.tools.bzip2._
+import org.clapper.argot._
+import ArgotConverters._
+
+object FilterGeotaggedWiki extends App {
+  val parser = new ArgotParser("textgrounder run opennlp.textgrounder.tr.app.FilterGeotaggedWiki", preUsage = Some("Textgrounder"))
+
+  val wikiTextInputFile = parser.option[String](List("w", "wiki"), "wiki", "wiki text input file")
+  val wikiCorpusInputFile = parser.option[String](List("c", "corpus"), "corpus", "wiki corpus input file")
+
+  try {
+    parser.parse(args)
+  }
+  catch {
+    case e: ArgotUsageException => println(e.message); sys.exit(0)
+  }
+
+  val ids = new collection.mutable.HashSet[Int]
+
+  val fis = new FileInputStream(wikiCorpusInputFile.value.get)
+  fis.read; fis.read
+  val cbzis = new CBZip2InputStream(fis)
+  val in = new BufferedReader(new InputStreamReader(cbzis))
+  var curLine = in.readLine
+  while(curLine != null) {
+    ids += curLine.split("\t")(0).toInt
+    curLine = in.readLine
+  }
+  in.close
+
+  val recognizer = new OpenNLPRecognizer
+  val tokenizer = new OpenNLPTokenizer
+
+  val wikiTextCorpus = Corpus.createStreamCorpus
+
+  wikiTextCorpus.addSource(new WikiTextSource(new BufferedReader(new FileReader(wikiTextInputFile.value.get))))
+  wikiTextCorpus.setFormat(BaseApp.CORPUS_FORMAT.WIKITEXT)
+
+  for(doc <- wikiTextCorpus) {
+    if(ids contains doc.getId) {
+      println("Article title: " + doc.getId)
+      println("Article ID: " + doc.title)
+      for(sent <- doc) {
+        for(token <- sent) {
+          println(token.getOrigForm)
+        }
+      }
+    }
+    else {
+      for(sent <- doc) { for(token <- sent) {} }
+    }
+  }
+}
