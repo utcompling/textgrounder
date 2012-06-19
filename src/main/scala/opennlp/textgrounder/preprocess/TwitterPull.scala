94113876b36fed07e99068efd94decbed01abec0
Ben Wing <ben@benwing.com>
2012-06-19 15:08:59 -0400

    Add comments and rewrite TwitterPull for timeslice as key (not tested)

--- src/main/scala/opennlp/textgrounder/preprocess/TwitterPull.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/TwitterPull.scala
@@ -8,24 +8,63 @@ import java.io._
 import java.lang.Double.isNaN
 import java.text.{SimpleDateFormat, ParseException}
 
+import util.control.Breaks._
+
 import opennlp.textgrounder.util.Twokenize
 
 /*
  * This program takes, as input, files which contain one tweet
  * per line in json format as directly pulled from the twitter
- * api. It outputs a folder that may be used as the
- * --input-corpus argument of tg-geolocate.
+ * API. It outputs a folder that may be used as the --input-corpus
+ * argument of tg-geolocate.  This is in "TextGrounder corpus" format,
+ * with one document per line, fields separated by tabs, and all the words and
+ * counts placed in a single field, of the form "WORD1:COUNT1 WORD2:COUNT2 ...".
+ * The fields currently output are:
+ *
+ * 1. user
+ * 2. timestamp
+ * 3. latitude
+ * 4. longitude
+ * 5. number of followers (people following the user)
+ * 6. number of people the user is following
+ * 7. number of tweets merged to form the per-user document
+ *
+ * No schema is generated; it must be created by hand.
+ *
+ * FIXME: The corpus is supposed to have latitude and longitude as a single
+ * "coordinate" field, but they appear to be separate fields here.
+ *
+ * The code first merges by user, using the earliest geolocated tweet as the
+ * user's location; tweets with a bounding box as their location rather than a
+ * single point are treated as if they have no location.  Then the code filters
+ * out users that have no geolocation or have a geolocation outside of North
+ * America (according to a crude bounding box), as well as those that are identified
+ * as likely "spammers" according to their number of followers and number of
+ * people they are following.
  */
 
+class Options(val keytype: String, val timeslice: Double) { }
+
 object TwitterPull {
-  // tweet = (Timestamp, Text, Lat, Lng, Followers, Following, Number of tweets (since we merge tweets)
-  type Tweet = (Int, String, Double, Double, Int, Int, Int)
-  // record = (username, Tweet, # of tweets represented)
+  // Tweet = Data for a tweet other than the tweet ID =
+  // (timestamp, text, lat, lng, followers, following, number of tweets)
+  // Note that we have "number of tweets" since we merge multiple tweets into
+  // a document, and also use type Tweet for them.
+  type Tweet = (String, Double, String, Double, Double, Int, Int, Int)
+  // TweetID = numeric string used to uniquely identify a tweet.
+  type TweetID = String
+  // Record = Data for tweet along with the key (e.g. username, timestamp) =
+  // (key, tweet data)
   type Record = (String, Tweet)
-  // author = (username, earliest timestamp, best lat, best lng, max followers, max following, number of tweets pulled)
-  type Author = (String, Int, Double, Double, Int, Int, Int)
-  type AuthorWord = (String, String)
-  // wordcount = (word, number of ocurrences)
+  // IDRecord = Tweet ID along with all other data for a tweet.
+  type IDRecord = (TweetID, Record)
+  // TweetNoText = Data for a merged set of tweets other than the text =
+  // (username, earliest timestamp, best lat, best lng, max followers,
+  //    max following, number of tweets pulled)
+  type TweetNoText = (String, Double, Double, Double, Int, Int, Int)
+  // KeyWord = (username, word)
+  type KeyWord = (String, String)
+  // WordCount = (word, number of ocurrences)
   type WordCount = (String, Long)
 
   def force_value(value: json.JValue): String = {
@@ -35,20 +74,31 @@ object TwitterPull {
         (value values) toString
   }
 
-  def parse_time(timestring: String): Int = {
+  /**
+   * Convert a Twitter timestamp, e.g. "Tue Jun 05 14:31:21 +0000 2012", into
+   * a time in seconds since the Epoch (Jan 1 1970, or so).
+   */
+  def parse_time(timestring: String): Double = {
     val sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy")
     try {
       sdf.parse(timestring)
-      (sdf.getCalendar.getTimeInMillis / 1000).toInt
+      sdf.getCalendar.getTimeInMillis / 1000
     } catch {
-      case pe: ParseException => 0
+      case pe: ParseException => 0.0
     }
   }
 
-  def is_valid_tweet(id_r: (String, Record)): Boolean = {
+  /**
+   * Return true if this tweet is "valid" in that it doesn't have any
+   * out-of-range values (blank strings or 0-valued quantities).  Note
+   * that we treat a case where both latitude and longitude are 0 as
+   * invalid even though technically such a place could exist. (FIXME,
+   * use NaN or something to indicate a missing latitude or longitude).
+   */
+  def is_valid_tweet(id_r: IDRecord): Boolean = {
     // filters out invalid tweets, as well as trivial spam
-    val (tw_id, (a, (ts, text, lat, lng, fers, fing, numtw))) = id_r
-    tw_id != "" && ts != 0 && a != "" && !(lat == 0.0 && lng == 0.0)
+    val (tw_id, (key, (user, ts, text, lat, lng, fers, fing, numtw))) = id_r
+    tw_id != "" && ts != 0.0 && user != "" && !(lat == 0.0 && lng == 0.0)
   }
 
   val MAX_NUMBER_FOLLOWING = 1000
@@ -56,8 +106,25 @@ object TwitterPull {
   val MIN_NUMBER_FOLLOWERS = 10
   val MIN_NUMBER_TWEETS = 10
   val MAX_NUMBER_TWEETS = 1000
+  /**
+   * Return true if this tweet combination (tweets for a given user)
+   * appears to reflect a "spammer" user or some other user with
+   * sufficiently nonstandard behavior that we want to exclude them (e.g.
+   * a celebrity or an inactive user): Having too few or too many tweets,
+   * following too many or too few, or having too few followers.  A spam
+   * account is likely to have too many tweets -- and even more, to send
+   * tweets to too many people (although we don't track this).  A spam
+   * account sends much more than it receives, and may have no followers.
+   * A celebrity account receives much more than it sends, and tends to have
+   * a lot of followers.  People who send too few tweets simply don't
+   * provide enough data.
+   *
+   * FIXME: We don't check for too many followers of a given account, but
+   * instead too many people that a given account is following.  Perhaps
+   * this is backwards?
+   */
   def is_nonspammer(r: Record): Boolean = {
-    val (a, (ts, text, lat, lng, fers, fing, numtw)) = r
+    val (key, (user, ts, text, lat, lng, fers, fing, numtw)) = r
 
     (fing >= MIN_NUMBER_FOLLOWING && fing <= MAX_NUMBER_FOLLOWING) &&
       (fers >= MIN_NUMBER_FOLLOWERS) &&
@@ -69,20 +136,31 @@ object TwitterPull {
   val MIN_LNG = -126.0
   val MAX_LAT = 49.0
   val MAX_LNG = -60.0
+  /**
+   * Return true of this tweet (combination) is located within the
+   * bounding box fo North America.
+   */
   def northamerica_only(r: Record): Boolean = {
-    val (a, (ts, text, lat, lng, fers, fing, numtw)) = r
+    val (key, (user, ts, text, lat, lng, fers, fing, numtw)) = r
 
     (lat >= MIN_LAT && lat <= MAX_LAT) &&
       (lng >= MIN_LNG && lng <= MAX_LNG)
   }
 
 
-  val empty_tweet: (String, Record) = ("", ("", (0, "", Double.NaN, Double.NaN, 0, 0, 0)))
+  /**
+   * An empty tweet, stored as a full IDRecord.
+   */
+  val empty_tweet: IDRecord = ("", ("", ("", 0.0, "", Double.NaN, Double.NaN, 0, 0, 0)))
 
-  def parse_json(line: String): (String, Record) = {
+  /**
+   * Parse a JSON line into a tweet.  Return value is an IDRecord, including
+   * the tweet ID, username, text and all other data.
+   */
+  def parse_json(line: String, opts: Options): IDRecord = {
     try {
       val parsed = json.parse(line)
-      val author = force_value(parsed \ "user" \ "screen_name")
+      val user = force_value(parsed \ "user" \ "screen_name")
       val timestamp = parse_time(force_value(parsed \ "created_at"))
       val text = force_value(parsed \ "text").replaceAll("\\s+", " ")
       val followers = force_value(parsed \ "user" \ "followers_count").toInt
@@ -97,7 +175,11 @@ object TwitterPull {
             (parsed \ "coordinates" \ "coordinates" values).asInstanceOf[List[Number]]
           (latlng(1).doubleValue, latlng(0).doubleValue)
         }
-      (tweet_id, (author, (timestamp, text, lat, lng, followers, following, 1)))
+      val key = opts.keytype match {
+        case "user" => user
+        case _ => ((timestamp / opts.timeslice).toInt * opts.timeslice).toString
+      }
+      (tweet_id, (key, (user, timestamp, text, lat, lng, followers, following, 1)))
     } catch {
       case jpe: json.JsonParser.ParseException => empty_tweet
       case npe: NullPointerException => empty_tweet
@@ -105,14 +187,32 @@ object TwitterPull {
     }
   }
 
-  def tweet_once(id_rs: (String, Iterable[Record])): Record = {
+  // Select the first tweet with the same ID.  For various reasons we may
+  // have duplicates of the same tweet among our data.  E.g. it seems that
+  // Twitter itself sometimes streams duplicates through its Streaming API,
+  // and data from different sources will almost certainly have duplicates.
+  // Furthermore, sometimes we want to get all the tweets even in the presence
+  // of flakiness that causes Twitter to sometimes bomb out in a Streaming
+  // session and take a while to restart, so we have two or three simultaneous
+  // streams going recording the same stuff, hoping that Twitter bombs out at
+  // different points in the different sessions (which is generally true).
+  // Then, all or almost all the tweets are available in the different streams,
+  // but there is a lot of duplication that needs to be tossed aside.
+  def tweet_once(id_rs: (TweetID, Iterable[Record])): Record = {
     val (id, rs) = id_rs
     rs.head
   }
 
+  /**
+   * Merge the data associated with two tweets or tweet combinations
+   * into a single tweet combination.  Concatenate text.  Find maximum
+   * numbers of followers and followees.  Add number of tweets in each.
+   * For latitude and longitude, take the earliest provided values
+   * ("earliest" by timestamp and "provided" meaning not missing).
+   */
   def merge_records(tweet1: Tweet, tweet2: Tweet): Tweet = {
-    val (ts1, text1, lat1, lng1, fers1, fing1, numtw1) = tweet1
-    val (ts2, text2, lat2, lng2, fers2, fing2, numtw2) = tweet2
+    val (user1, ts1, text1, lat1, lng1, fers1, fing1, numtw1) = tweet1
+    val (user2, ts2, text2, lat2, lng2, fers2, fing2, numtw2) = tweet2
     val (fers, fing) = (math.max(fers1, fers2), math.max(fing1, fing2))
     val text = text1 + " " + text2
     val numtw = numtw1 + numtw2
@@ -130,18 +230,29 @@ object TwitterPull {
         (lat2, lng2, ts2)
       }
 
-    (ts, text, lat, lng, fers, fing, numtw)
+    // FIXME maybe want to track the different users
+    (user1, ts, text, lat, lng, fers, fing, numtw)
   }
 
+  /**
+   * Return true if tweet (combination) has a fully-specified latitude
+   * and longitude.
+   */
   def has_latlng(r: Record): Boolean = {
-    val (a, (ts, text, lat, lng, fers, fing, numtw)) = r
+    val (key, (user, ts, text, lat, lng, fers, fing, numtw)) = r
     !isNaN(lat) && !isNaN(lng)
   }
 
+  /**
+   * Convert a word to lowercase.
+   */
   def normalize_word(word: String): String = {
     word.toLowerCase
   }
 
+  /**
+   * Use Twokenize to break up a tweet into separate tokens.
+   */
   def tokenize(text: String): Iterable[String] = {
     return Twokenize(text).map(normalize_word)
   }
@@ -153,27 +264,28 @@ object TwitterPull {
   }
 
   def checkpoint_str(r: Record): String = {
-    val (author, (ts, text, lat, lng, fers, fing, numtw)) = r
+    val (key, (user, ts, text, lat, lng, fers, fing, numtw)) = r
     val text_2 = text.replaceAll("\\s+", " ")
-    val s = Seq(author, ts, lat, lng, fers, fing, numtw, "", text_2) mkString "\t"
+    val s = Seq(key, user, ts, lat, lng, fers, fing, numtw, "", text_2) mkString "\t"
     s
   }
 
   def from_checkpoint_to_record(line: String): Record = {
     val split = line.split("\t\t")
-    val (author_data, text) = (split(0), split(1))
-    val split2 = author_data.split("\t")
-    val author = split2(0)
-    val ts = split2(1).toInt
-    val lat = split2(2).toDouble
-    val lng = split2(3).toDouble
-    val fers = split2(4).toInt
-    val fing = split2(5).toInt
-    val numtw = split2(6).toInt
-    (author, (ts, text, lat, lng, fers, fing, numtw))
+    val (tweet_no_text, text) = (split(0), split(1))
+    val split2 = tweet_no_text.split("\t")
+    val key = split2(0)
+    val user = split2(1)
+    val ts = split2(2).toDouble
+    val lat = split2(3).toDouble
+    val lng = split2(4).toDouble
+    val fers = split2(5).toInt
+    val fing = split2(6).toInt
+    val numtw = split2(7).toInt
+    (key, (user, ts, text, lat, lng, fers, fing, numtw))
   }
 
-  def from_checkpoint_to_author_text(line: String): (String, String) = {
+  def from_checkpoint_to_tweet_text(line: String): (String, String) = {
     val split = line.split("\t\t")
     if (split.length != 2) {
       System.err.println("Bad line: " + line)
@@ -183,24 +295,39 @@ object TwitterPull {
     }
   }
 
-  def emit_words(author_text: (String, String)): Iterable[(AuthorWord, Long)] = {
-    val (authordata, text) = author_text
+  def emit_words(tweet_text: (String, String)): Iterable[(KeyWord, Long)] = {
+    val (tweet_no_text, text) = tweet_text
     for (word <- tokenize(text) if !filter_word(word))
-      yield ((authordata, word), 1L)
+      yield ((tweet_no_text, word), 1L)
   }
 
-  def reposition_word(awc: (AuthorWord, Long)): (String, WordCount) = {
-    val ((author, word), c) = awc
-    (author, (word, c))
+  def reposition_word(kwc: (KeyWord, Long)): (String, WordCount) = {
+    val ((key, word), c) = kwc
+    (key, (word, c))
   }
 
-  def nicely_format_plain(awcs: (String, Iterable[WordCount])): String = {
-    val (author, wcs) = awcs
+  def nicely_format_plain(kwcs: (String, Iterable[WordCount])): String = {
+    val (key, wcs) = kwcs
     val nice_text = wcs.map((w: WordCount) => w._1 + ":" + w._2).mkString(" ")
-    author + "\t" + nice_text
+    key + "\t" + nice_text
   }
 
-  def main(args: Array[String]) = withHadoopArgs(args) { a =>
+  def main(orig_args: Array[String]) = withHadoopArgs(orig_args) { args =>
+    var by_time = false
+    var timeslice = 6.0
+    var a = args
+
+    breakable {
+      while (a.length > 0) {
+        if (a(0) == "--by-time") {
+          by_time = true
+          a = a.tail
+        } else if (a(0) == "--timeslice" || a(0) == "--time-slice") {
+          timeslice = a(1).toDouble
+          a = a.tail.tail
+        } else break
+      }
+    }
 
     // make sure we get all the input
     val (inputPath, outputPath) =
@@ -210,47 +337,70 @@ object TwitterPull {
         sys.error("Expecting input and output path.")
       }
 
-    // Firstly we load up all the (new-line-seperated) json lines
+    val opts = new Options(if (by_time) "timestamp" else "user", timeslice)
+
+    // Firstly we load up all the (new-line-separated) JSON lines.
     val lines: DList[String] = TextInput.fromTextFile(inputPath)
 
-    // Filter out some trivially invalid tweets and parse the json
-    val values_extracted = lines.map(parse_json).filter(is_valid_tweet)
+    // Parse JSON into tweet records (IDRecord), filter out invalid tweets.
+    val values_extracted = lines.map(parse_json(_, opts)).filter(is_valid_tweet)
 
+    // Filter out duplicate tweets -- group by Tweet ID and then take the
+    // first tweet for a given ID.  Duplicate tweets occur for various
+    // reasons, e.g. sometimes in the stream itself due to Twitter errors,
+    // or when combining data from multiple, possibly overlapping, sources.
+    // In the process, the tweet ID's are discarded.
     val single_tweets = values_extracted.groupByKey.map(tweet_once)
 
+    // Checkpoint the resulting tweets (minus ID) onto disk.
     val checkpoint1 = single_tweets.map(checkpoint_str)
     DList.persist(TextOutput.toTextFile(checkpoint1, outputPath + "-st"))
 
+    // Then load back up.
     val lines2: DList[String] = TextInput.fromTextFile(outputPath + "-st")
     val values_extracted2 = lines2.map(from_checkpoint_to_record)
 
-    // group by author, combine the records, keeping the earliest
-    // tweet with a specific coordinate
+    // Group by username, then combine the tweets for a user into a
+    // tweet combination, with text concatenated and the location taken
+    // from the earliest/ tweet with a specific coordinate.
     val concatted = values_extracted2.groupByKey.combine(merge_records)
 
-    // filter every user that doesn't have a specific coordinate
+    // Filter the tweet combinations, removing users without a specific
+    // coordinate; users that appear to be "spammers" or other users with
+    // non-standard behavior; and users located outside of North America.
     val with_coord = concatted.filter(has_latlng)
                               .filter(is_nonspammer)
                               .filter(northamerica_only)
 
 
+    // Checkpoint a second time.
     val checkpoint = with_coord.map(checkpoint_str)
     DList.persist(TextOutput.toTextFile(checkpoint, outputPath + "-cp"))
 
-    // load from the checkpoint
+    // Load from second checkpoint.  Note that each time we checkpoint,
+    // we extract the "text" field and stick it at the end.  This time
+    // when loading it up, we use `from_checkpoint_to_tweet_text`, which
+    // gives us the tweet data as a string (minus text) and the text, as
+    // two separate strings.
     val lines_cp: DList[String] = TextInput.fromTextFile(outputPath + "-cp")
-    // word count
-    val emitted_words = lines_cp.map(from_checkpoint_to_author_text)
+
+    // Now word count.  We run `from_checkpoint_to_tweet_text` (see above)
+    // to get separate access to the text, then Twokenize it into words
+    // and emit a series of key-value pairs of (word, count).
+    val emitted_words = lines_cp.map(from_checkpoint_to_tweet_text)
                                 .flatMap(emit_words)
+    // Group based on key (the word) and combine by adding up the individual
+    // counts.
     val word_counts = emitted_words.groupByKey.combine((a: Long, b: Long) => (a + b))
 
-    // regroup with author as key, word pairs as values
-    val regrouped_by_author = word_counts.map(reposition_word).groupByKey
+    // Regroup with proper key (user, timestamp, etc.) as key,
+    // word pairs as values.
+    val regrouped_by_key = word_counts.map(reposition_word).groupByKey
 
-    // nice string output
-    val nicely_formatted = regrouped_by_author.map(nicely_format_plain)
+    // Nice string output.
+    val nicely_formatted = regrouped_by_key.map(nicely_format_plain)
 
-    // save to disk
+    // Save to disk.
     DList.persist(TextOutput.toTextFile(nicely_formatted, outputPath))
   }
 }
