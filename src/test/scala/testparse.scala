f94ad84c2fd9f4308d1f0bcb14c66cdaa40f61fa
Ben Wing <ben@benwing.com>
2012-07-02 03:40:13 -0400

    Fifth working version of Parser Combinator test implementation

--- src/test/scala/testparse.scala
+++ src/test/scala/testparse.scala
@@ -1,31 +1,6 @@
+import scala.util.parsing.combinator.lexical.StdLexical
 import scala.util.parsing.combinator.syntactical._
-import scala.util.parsing.combinator.lexical._
-import scala.util.parsing.combinator.token._
-import scala.collection.mutable.HashSet
 import scala.util.parsing.input.CharArrayReader.EofCh
-
-object ArithmeticParser extends StdTokenParsers with Application {
- type Tokens = StdLexical
-
- val lexical = new StdLexical
-
- lexical.delimiters ++= List("(", ")", "+", "-", "*", "/")
-
- def factor: Parser[Int] = "(" ~> expr <~ ")" | numericLit ^^ (_.toInt)
-
- def term : Parser[Int] = (
-   factor ~ "*" ~ term ^^ { case x ~ "*" ~ y => x * y } |
-   factor ~ "/" ~ term ^^ { case x ~ "/" ~ y => x / y } | factor )
-
-def expr : Parser[Int] = (
-  term ~ "+" ~ expr ^^ { case x ~ "+" ~ y => x + y } |
-  term ~ "-" ~ expr ^^ { case x ~ "-" ~ y => x - y } | term )
-
-  def apply(x: String) =
-    //(expr (new lexical.Scanner ("1+2*3*7-1") ))
-    (expr (new lexical.Scanner (x) ))
-}
-
 
 sealed abstract class Expr {
   def matches(x: Seq[String]): Boolean
@@ -47,36 +22,34 @@ case class ENot(e:Expr) extends Expr {
   def matches(x: Seq[String]) = !e.matches(x)
 }
 
-import scala.util.parsing.combinator.lexical.StdLexical
-
 class ExprLexical extends StdLexical {
-    override def token: Parser[Token] = floatingToken | super.token
+  override def token: Parser[Token] = floatingToken | super.token
 
-    def floatingToken: Parser[Token] =
-        rep1(digit) ~ optFraction ~ optExponent ^^
-            { case intPart ~ frac ~ exp => NumericLit(
-                    (intPart mkString "") :: frac :: exp :: Nil mkString "")}
+  def floatingToken: Parser[Token] =
+    rep1(digit) ~ optFraction ~ optExponent ^^
+      { case intPart ~ frac ~ exp => NumericLit(
+          (intPart mkString "") :: frac :: exp :: Nil mkString "")}
 
-    def chr(c:Char) = elem("", ch => ch==c )
-    def sign = chr('+') | chr('-')
-    def optSign = opt(sign) ^^ {
-        case None => ""
-        case Some(sign) => sign
-    }
-    def fraction = '.' ~ rep(digit) ^^ {
-        case dot ~ ff => dot :: (ff mkString "") :: Nil mkString ""
-    }
-    def optFraction = opt(fraction) ^^ {
-        case None => ""
-        case Some(fraction) => fraction
-    }
-    def exponent = (chr('e') | chr('E')) ~ optSign ~ rep1(digit) ^^ {
-        case e ~ optSign ~ exp => e :: optSign :: (exp mkString "") :: Nil mkString ""
-    }
-    def optExponent = opt(exponent) ^^ {
-        case None => ""
-        case Some(exponent) => exponent
-    }
+  def chr(c:Char) = elem("", ch => ch==c )
+  def sign = chr('+') | chr('-')
+  def optSign = opt(sign) ^^ {
+    case None => ""
+    case Some(sign) => sign
+  }
+  def fraction = '.' ~ rep(digit) ^^ {
+    case dot ~ ff => dot :: (ff mkString "") :: Nil mkString ""
+  }
+  def optFraction = opt(fraction) ^^ {
+    case None => ""
+    case Some(fraction) => fraction
+  }
+  def exponent = (chr('e') | chr('E')) ~ optSign ~ rep1(digit) ^^ {
+    case e ~ optSign ~ exp => e :: optSign :: (exp mkString "") :: Nil mkString ""
+  }
+  def optExponent = opt(exponent) ^^ {
+    case None => ""
+    case Some(exponent) => exponent
+  }
 }
 
 class FilterLexical extends StdLexical {
@@ -111,57 +84,55 @@ class FilterLexical extends StdLexical {
     if (reserved contains name) Keyword(name) else StringLit(name)
 }
 
-import scala.util.parsing.combinator.syntactical._
+object FilterParser extends StandardTokenParsers {
+  override val lexical = new FilterLexical
+  lexical.reserved ++= List("AND", "OR", "NOT")
+  // lexical.delimiters ++= List("&","|","!","(",")")
+  lexical.delimiters ++= List("(",")")
 
-object ExprParser extends StandardTokenParsers {
-    override val lexical = new FilterLexical
-    lexical.reserved ++= List("&", "|", "!")
-    // lexical.delimiters ++= List("&","|","!","(",")")
-    lexical.delimiters ++= List("(",")")
+  def word = stringLit ^^ { s => EConst(Seq(s)) }
 
-    def word = stringLit ^^ { s => EConst(Seq(s)) }
-
-    def words =
-        word.+ ^^ { x => EConst(x.flatMap(_ match { case EConst(y) => y })) }
+  def words =
+    word.+ ^^ { x => EConst(x.flatMap(_ match { case EConst(y) => y })) }
 
 
-    def parens: Parser[Expr] = "(" ~> expr <~ ")"
+  def parens: Parser[Expr] = "(" ~> expr <~ ")"
 
-    def not: Parser[ENot] = "!" ~> term ^^ { ENot(_) }
+  def not: Parser[ENot] = "NOT" ~> term ^^ { ENot(_) }
 
-    def term = ( words | parens | not )
+  def term = ( words | parens | not )
 
-    def andexpr = term * (
-            "&" ^^^ { (a:Expr, b:Expr) => EAnd(a,b) } )
+  def andexpr = term * (
+    "AND" ^^^ { (a:Expr, b:Expr) => EAnd(a,b) } )
 
-    def orexpr = andexpr * (
-            "|" ^^^ { (a:Expr, b:Expr) => EOr(a,b) } )
+  def orexpr = andexpr * (
+    "OR" ^^^ { (a:Expr, b:Expr) => EOr(a,b) } )
 
-    def expr = ( orexpr | term )
+  def expr = ( orexpr | term )
 
-    def parse(s :String) = {
-        val tokens = new lexical.Scanner(s)
-        phrase(expr)(tokens)
+  def parse(s :String) = {
+    val tokens = new lexical.Scanner(s)
+    phrase(expr)(tokens)
+  }
+
+  def apply(s: String): Expr = {
+    parse(s) match {
+      case Success(tree, _) => tree
+      case e: NoSuccess =>
+        throw new IllegalArgumentException("Bad syntax: "+s)
     }
+  }
 
-    def apply(s: String): Expr = {
-        parse(s) match {
-            case Success(tree, _) => tree
-            case e: NoSuccess =>
-                   throw new IllegalArgumentException("Bad syntax: "+s)
-        }
+  def test(exprstr: String, tweet: Seq[String]) = {
+    parse(exprstr) match {
+      case Success(tree, _) =>
+        println("Tree: "+tree)
+        val v = tree.matches(tweet)
+        println("Eval: "+v)
+      case e: NoSuccess => Console.err.println(e)
     }
-
-    def test(exprstr: String, tweet: Seq[String]) = {
-        parse(exprstr) match {
-            case Success(tree, _) =>
-                println("Tree: "+tree)
-                val v = tree.matches(tweet)
-                println("Eval: "+v)
-            case e: NoSuccess => Console.err.println(e)
-        }
-    }
-    
-    //A main method for testing
-    def main(args: Array[String]) = test(args(0), args(1).split("""\s"""))
+  }
+  
+  //A main method for testing
+  def main(args: Array[String]) = test(args(0), args(1).split("""\s"""))
 }
