41b0c0cb21dc85ce20594f18e6e5057d86bd0320
Ben Wing <ben@benwing.com>
2012-07-13 05:26:37 -0400

    Add ScoobiWordCount.scala for testing

---
+++ src/main/scala/opennlp/textgrounder/preprocess/ScoobiWordCount.scala
@@ -0,0 +1,26 @@
+package opennlp.textgrounder.preprocess
+
+import com.nicta.scoobi.Scoobi._
+import com.nicta.scoobi.testing._
+import org.apache.commons.logging.LogFactory
+import java.io._
+
+object ScoobiWordCount extends ScoobiApp {
+  def run() {
+    val lines = fromTextFile(args(0))
+
+    def splitit(x: String) = {
+      HadoopLogFactory.setQuiet(false)
+      val logger = LogFactory.getLog("foo.bar")
+      logger.info("Processing " + x)
+      // System.err.println("Processing", x)
+      x.split(" ")
+    }
+    //val counts = lines.flatMap(_.split(" "))
+    val counts = lines.flatMap(splitit)
+                          .map(word => (word, 1))
+                          .groupByKey
+                          .combine((a: Int, b: Int) => a + b)
+    persist(toTextFile(counts, args(1)))
+  }
+}
