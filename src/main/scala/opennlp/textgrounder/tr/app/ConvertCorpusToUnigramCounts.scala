6d6cc51434900075d782beba140f13c5385d0bfe
Mike Speriosu <mike@subtextsolutions.com>
2012-08-01 03:57:56 -0500

    Made plaintext-to-unigram-counts converter more general; added 'viz' command line shortcut.

--- src/main/scala/opennlp/textgrounder/tr/app/ConvertCorpusToUnigramCounts.scala
+++ src/main/scala/opennlp/textgrounder/tr/app/ConvertCorpusToUnigramCounts.scala
@@ -12,61 +12,75 @@ import opennlp.textgrounder.tr.text.io._
 
 import scala.collection.JavaConversions._
 
-object ConvertCorpusToUnigramCounts extends App {
+object ConvertCorpusToUnigramCounts extends BaseApp {
 
   val alphanumRE = """^[a-z0-9]+$""".r
 
-  val tokenizer = new OpenNLPTokenizer
+  //val tokenizer = new OpenNLPTokenizer
 
-  var corpus = Corpus.createStoredCorpus
-  if(args(0).endsWith("txt")) {
-    /*
-    val tokenizer = new OpenNLPTokenizer
-    //val recognizer = new OpenNLPRecognizer
-    //val gis = new GZIPInputStream(new FileInputStream(args(1)))
-    //val ois = new ObjectInputStream(gis)
-    //val gnGaz = ois.readObject.asInstanceOf[GeoNamesGazetteer]
-    //gis.close
-    corpus.addSource(new PlainTextSource(
-      new BufferedReader(new FileReader(args(0))), new OpenNLPSentenceDivider(), tokenizer))
-    //corpus.addSource(new ToponymAnnotator(new PlainTextSource(
-    //		new BufferedReader(new FileReader(args(0))), new OpenNLPSentenceDivider(), tokenizer),
-    //          recognizer, gnGaz, null))
-    corpus.setFormat(BaseApp.CORPUS_FORMAT.PLAIN)
-    */
-    val importCorpus = new ImportCorpus
-    corpus = importCorpus.doImport(args(0), args(1), BaseApp.CORPUS_FORMAT.PLAIN, false)
-  }
-  else {
-    corpus.addSource(new TrXMLDirSource(new File(args(0)), tokenizer))
-    corpus.setFormat(BaseApp.CORPUS_FORMAT.TRCONLL)
-    corpus.load
-  }
-  //corpus.load
+  def main(args:Array[String]) { 
 
-  System.err.println(corpus.size)
+    initializeOptionsFromCommandLine(args);
 
-  for(doc <- corpus) {
-    val unigramCounts = new collection.mutable.HashMap[String, Int]
-    for(sent <- doc) {
-      for(rawToken <- sent) {
-        for(token <- rawToken.getForm.split(" ")) {
-          val ltoken = token.toLowerCase
-          if(alphanumRE.findFirstIn(ltoken) != None) {
-            val prevCount = unigramCounts.getOrElse(ltoken, 0)
-            unigramCounts.put(ltoken, prevCount + 1)
+    /*var corpus = Corpus.createStoredCorpus
+
+    if(getCorpusFormat == BaseApp.CORPUS_FORMAT.PLAIN/**/) {
+      /*
+      val tokenizer = new OpenNLPTokenizer
+      //val recognizer = new OpenNLPRecognizer
+      //val gis = new GZIPInputStream(new FileInputStream(args(1)))
+      //val ois = new ObjectInputStream(gis)
+      //val gnGaz = ois.readObject.asInstanceOf[GeoNamesGazetteer]
+      //gis.close
+      corpus.addSource(new PlainTextSource(
+        new BufferedReader(new FileReader(args(0))), new OpenNLPSentenceDivider(), tokenizer))
+      //corpus.addSource(new ToponymAnnotator(new PlainTextSource(
+      //		new BufferedReader(new FileReader(args(0))), new OpenNLPSentenceDivider(), tokenizer),
+      //          recognizer, gnGaz, null))
+      corpus.setFormat(BaseApp.CORPUS_FORMAT.PLAIN)
+      */
+      val importCorpus = new ImportCorpus
+      //if(args(0).endsWith("txt"))
+      corpus = importCorpus.doImport(getCorpusInputPath, , getCorpusFormat, false)
+      //else
+      //  corpus = importCorpus
+    }
+    else if(getCorpusFormat == BaseApp.CORPUS_FORMAT.TRCONLL) {
+      corpus.addSource(new TrXMLDirSource(new File(args(0)), tokenizer))
+      corpus.setFormat(BaseApp.CORPUS_FORMAT.TRCONLL)
+      corpus.load
+    }
+    //corpus.load*/
+
+    val corpus = TopoUtil.readStoredCorpusFromSerialized(getSerializedCorpusInputPath)
+    
+    System.err.println(corpus.size)
+    
+    var i = 0
+    for(doc <- corpus) {
+      val unigramCounts = new collection.mutable.HashMap[String, Int]
+      for(sent <- doc) {
+        for(rawToken <- sent) {
+          for(token <- rawToken.getForm.split(" ")) {
+            val ltoken = token.toLowerCase
+            if(alphanumRE.findFirstIn(ltoken) != None) {
+              val prevCount = unigramCounts.getOrElse(ltoken, 0)
+              unigramCounts.put(ltoken, prevCount + 1)
+            }
           }
         }
       }
+    
+      print(i/*doc.getId.drop(1)*/ +"\t")
+      print(doc.getId+"\t")
+      print("0,0\t")
+      print("1\t\tMain\tno\tno\tno\t")
+      for((word, count) <- unigramCounts) {
+        print(word+":"+count+" ")
+      }
+      println
+      i += 1
     }
 
-    print(doc.getId.drop(1)+"\t")
-    print(doc.getId+"\t")
-    print("0,0\t")
-    print("1\t\tMain\tno\tno\tno\t")
-    for((word, count) <- unigramCounts) {
-      print(word+":"+count+" ")
-    }
-    println
   }
 }
