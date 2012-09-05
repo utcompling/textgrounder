5487e61a1aadee726b426c32897ba797fe66d4aa
Ben Wing <ben@benwing.com>
2012-09-05 18:44:39 -0400

    GroupTwitterPull: comment out unused ExprLexical

--- src/main/scala/opennlp/textgrounder/preprocess/GroupTwitterPull.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/GroupTwitterPull.scala
@@ -210,36 +210,38 @@ object GroupTwitterPull extends ScoobiProcessFilesApp[GroupTwitterPullParams] {
       def check(x: Seq[String]) = !e.check(x)
     }
 
-    class ExprLexical extends StdLexical {
-      override def token: Parser[Token] = floatingToken | super.token
-
-      def floatingToken: Parser[Token] =
-        rep1(digit) ~ optFraction ~ optExponent ^^
-          { case intPart ~ frac ~ exp => NumericLit(
-              (intPart mkString "") :: frac :: exp :: Nil mkString "")}
-
-      def chr(c:Char) = elem("", ch => ch==c )
-      def sign = chr('+') | chr('-')
-      def optSign = opt(sign) ^^ {
-        case None => ""
-        case Some(sign) => sign
-      }
-      def fraction = '.' ~ rep(digit) ^^ {
-        case dot ~ ff => dot :: (ff mkString "") :: Nil mkString ""
-      }
-      def optFraction = opt(fraction) ^^ {
-        case None => ""
-        case Some(fraction) => fraction
-      }
-      def exponent = (chr('e') | chr('E')) ~ optSign ~ rep1(digit) ^^ {
-        case e ~ optSign ~ exp =>
-          e :: optSign :: (exp mkString "") :: Nil mkString ""
-      }
-      def optExponent = opt(exponent) ^^ {
-        case None => ""
-        case Some(exponent) => exponent
-      }
-    }
+    // NOT CURRENTLY USED, but potentially useful as an indicator of how to
+    // implement a parser for numbers.
+//    class ExprLexical extends StdLexical {
+//      override def token: Parser[Token] = floatingToken | super.token
+//
+//      def floatingToken: Parser[Token] =
+//        rep1(digit) ~ optFraction ~ optExponent ^^
+//          { case intPart ~ frac ~ exp => NumericLit(
+//              (intPart mkString "") :: frac :: exp :: Nil mkString "")}
+//
+//      def chr(c:Char) = elem("", ch => ch==c )
+//      def sign = chr('+') | chr('-')
+//      def optSign = opt(sign) ^^ {
+//        case None => ""
+//        case Some(sign) => sign
+//      }
+//      def fraction = '.' ~ rep(digit) ^^ {
+//        case dot ~ ff => dot :: (ff mkString "") :: Nil mkString ""
+//      }
+//      def optFraction = opt(fraction) ^^ {
+//        case None => ""
+//        case Some(fraction) => fraction
+//      }
+//      def exponent = (chr('e') | chr('E')) ~ optSign ~ rep1(digit) ^^ {
+//        case e ~ optSign ~ exp =>
+//          e :: optSign :: (exp mkString "") :: Nil mkString ""
+//      }
+//      def optExponent = opt(exponent) ^^ {
+//        case None => ""
+//        case Some(exponent) => exponent
+//      }
+//    }
 
     class FilterLexical extends StdLexical {
       // see `token` in `Scanners`
