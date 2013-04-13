a9ec40fefa16e1426f91973bcbc085d63ef60471
Ben Wing <ben@benwing.com>
2013-04-13 14:04:31 -0500

    GenerateTwitterUserLocationJS: Implement displaying paths of user movements

--- src/main/scala/opennlp/textgrounder/postprocess/GenerateTwitterUserLocationJS.scala
+++ src/main/scala/opennlp/textgrounder/postprocess/GenerateTwitterUserLocationJS.scala
@@ -33,6 +33,10 @@ class GenerateTwitterUserLocationJSParameters(ap: ArgParser) {
   var input = ap.positional[String]("input",
     help = "Results file to analyze.")
 
+  var path = ap.flag("path",
+      help = """Generate paths showing movement of user instead of just
+a single marker for the first location of the user.""")
+
   var debug =
     ap.option[String]("d", "debug", metavar = "FLAGS",
       help = """Output debug info of the given types.  Multiple debug
@@ -82,36 +86,35 @@ object GenerateTwitterUserLocationJS extends ExperimentApp("GenerateTwitterUserL
     val (dir, base) = filehand.split_filename(input_file)
     val (schema, field_iter) =
       TextDB.read_textdb_with_schema(filehand, dir, prefix = base)
-    val markers =
-      for {fieldvals <- field_iter.flatten
-           coord = schema.get_field(fieldvals, "coord")
-           if coord != ""
-           user = schema.get_field(fieldvals, "user")
-      } yield "[ %s, '%s' ]" format (coord, user)
-    val marker_text = markers mkString ",\n            "
-    uniprint(
+
+    val prelude =
 """<!DOCTYPE html>
 <html>
   <head>
-    <title>Marker example</title>
+    <title>Twitter user locations</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <style>
       html, body, #map-canvas {
         margin: 0;
         padding: 0;
-        height: 100%%;
+        height: 100%;
       }
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script>
       var markers = [];
+      var lines = [];
       var map;
+      // Convert to hex string, pad to specified length (default 2)
+      function toHex(num, len) {
+        len = (typeof len === "undefined") ? 2 : len;
+        var hex = Math.floor(num).toString(16);
+        while (hex.length < len)
+          hex = "0" + hex;
+        return hex;
+      }
       function initialize() {
-        var latlons = [
-          %s
-        ];
-
         var mapOptions = {
           zoom: 11,
           center: new google.maps.LatLng(37.78, -122.42),
@@ -119,7 +122,25 @@ object GenerateTwitterUserLocationJS extends ExperimentApp("GenerateTwitterUserL
         }
         map = new google.maps.Map(document.getElementById("map-canvas"),
           mapOptions);
+"""
 
+    val postlude = """
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
+  <body>
+    <div id="map-canvas"></div>
+  </body>
+</html>
+"""
+
+    val marker_code_data = """
+        var latlons = [
+          %s
+        ];
+"""
+    val marker_code_postlude = """
         for (var i = 0; i < latlons.length; i++) {
           var coord = latlons[i];
           var latlng = new google.maps.LatLng(coord[0], coord[1]);
@@ -129,16 +150,112 @@ object GenerateTwitterUserLocationJS extends ExperimentApp("GenerateTwitterUserL
             title:coord[2]
           }));
         }
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-  </head>
-  <body>
-    <div id="map-canvas"></div>
-  </body>
-</html>
-""" format marker_text)
+"""
 
+    val path_code_data = """
+        var user_paths = [
+          %s
+        ];
+"""
+    val path_code_postlude = """
+        var lineSymbol = {
+          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+        };
+
+        for (var i = 0; i < user_paths.length; i++) {
+          var user = user_paths[i][0];
+          var path = user_paths[i][1];
+
+          // create a marker for the starting position
+          var marker_pos = new google.maps.LatLng(path[0][0], path[0][1]);
+          var timestr = new Date(path[0][2]).toLocaleString();
+          markers.push(new google.maps.Marker({
+            position: marker_pos,
+            map: map,
+            title:user + " #0," + timestr
+          }));
+
+          // create the path coordinates
+          var path_coords = [];
+          for (var j = 0; j < path.length; j++) {
+            var path_coord = new google.maps.LatLng(path[j][0], path[j][1]);
+            path_coords.push(path_coord);
+          }
+          for (var j = 1; j < path.length - 1; j++) {
+            var path_coord = new google.maps.LatLng(path[j][0], path[j][1]);
+            // assign color based on sequence of positions, to distinguish them,
+            // ranging from #000000 to #FFFFFF
+            var varcolor = toHex(j * 256 / path.length);
+            var timestr = new Date(path[j][2]).toLocaleString();
+            markers.push(new google.maps.Marker({
+              position: path_coord,
+              icon: {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                //strokeColor: "gold",
+                strokeWeight: 0,
+                fillColor: "#" + varcolor + varcolor + varcolor,
+                fillOpacity: 0.8,
+                scale: 3
+              },
+              map: map,
+              title: user + " #" + j + ", " + timestr
+            }));
+          }
+
+          // assign color based on sequence of users, to distinguish them,
+          // ranging from #FF0000 to #00FFFF
+          var invcolor = toHex((user_paths.length - i) * 255 / user_paths.length);
+          var varcolor = toHex(i * 255 / user_paths.length);
+
+          // create the polyline, with an arrow at the end
+          lines.push(new google.maps.Polyline({
+            path: path_coords,
+            strokeColor: "#" + invcolor + varcolor + varcolor,
+            strokeOpacity: 1.0,
+            strokeWeight: 1,
+            icons: [{
+              icon: lineSymbol,
+              offset: '100%'
+            }],
+            map: map
+          }));
+        }
+"""
+
+    val indent12 = "\n            "
+    val indent14 = "\n              "
+    val middle_code =
+      if (params.path) {
+        val lines =
+          for {fieldvals <- field_iter.flatten
+               position_field = schema.get_field(fieldvals, "positions")
+               if position_field != ""
+               user = schema.get_field(fieldvals, "user")
+          } yield {
+            val positions = decode_string_map(position_field)
+            val position_text =
+              positions map {
+                case (time, coord) => "[ %s, %s ]" format (coord, time)
+              } mkString ("," + indent14)
+            ("[ '%s', [" + indent14 + "%s ] ]") format (user, position_text)
+          }
+        val path_text = lines mkString ("," + indent12)
+
+        (path_code_data format path_text) + path_code_postlude
+      } else {
+        val markers =
+          for {fieldvals <- field_iter.flatten
+               coord = schema.get_field(fieldvals, "coord")
+               if coord != ""
+               user = schema.get_field(fieldvals, "user")
+          } yield "[ %s, '%s' ]" format (coord, user)
+
+        val marker_text = markers mkString ("," + indent12)
+
+        (marker_code_data format marker_text) + marker_code_postlude
+      }
+
+    uniprint(prelude + middle_code + postlude)
     0
   }
 }
