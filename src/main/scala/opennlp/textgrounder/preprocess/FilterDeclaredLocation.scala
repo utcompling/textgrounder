7509dc661416828c6abd46981abeab78d39e45c7
Ben Wing <ben@benwing.com>
2014-06-30 18:34:37 -0500

    FilterDeclaredLocation: Finish app

--- src/main/scala/opennlp/textgrounder/preprocess/FilterDeclaredLocation.scala
+++ src/main/scala/opennlp/textgrounder/preprocess/FilterDeclaredLocation.scala
@@ -18,7 +18,8 @@
 package opennlp.textgrounder
 package preprocess
 
-import collection.mutable
+import scala.collection.mutable
+import scala.util.Random
 
 import net.liftweb
 
@@ -34,12 +35,24 @@ class FilterDeclaredLocationParameters(ap: ArgParser) {
     help = """Ambiguous city or cities that the location field must refer to.
 Multiple cities should be separated by commas. NOTE: Currently only supports
 single-word city names.""")
-  var location_file = ap.option[String]("location-file",
+  var filtered_tweet_file = ap.option[String]("filtered-tweet-file", "ftf",
     must = be_size_>[String](0), // Allows for unspecified file.
-    help = """File to which cleaned-up locations and counts are written to.""")
+    help = """File to which filtered tweets are written to.""")
   var input = ap.multiPositional[String]("input",
     must = be_specified,
     help = """Tweet file(s) to analyze.""")
+  var excluded_cities = ap.option[String]("excluded-cities",
+    default = "",
+    help = """Cities to exclude from filtered tweets. Separate with semicolons.
+Should be of the form 'springfield, pennsylvania', all in lowercase.""")
+  var max_users = ap.option[Int]("max-users",
+    must = be_>(0),
+    default = 100,
+    help = "Maximum number of users to select per city.")
+  var min_tweets = ap.option[Int]("min-tweets",
+    must = be_>(0),
+    default = 5,
+    help = "Minimum number of tweets for selecting users.")
 }
 
 /**
@@ -133,11 +146,18 @@ object FilterDeclaredLocation extends ExperimentApp("FilterDeclaredLocation") {
   }
 
   def run_program(args: Array[String]) = {
-    // Map from users to cities seen for that user
-    val users_cities = intmapmap[String, String]()
-    val city_counts = intmap[String]()
-    val cities = params.city.split(""",\s*""")
+    val excluded_cities = params.excluded_cities.split(";").toSet
+    // Map from users to map of cities seen for that user and # tweets seen
+    // for each city, where "city" is actually a city/state combination
+    val users_cities_tweetcounts_map = intmapmap[String, String]()
+    // Map of # tweets seen for each city overall, where "city" is
+    // actually a city/state combination
+    val cities_tweetcounts = intmap[String]()
+    // List of ambiguous cities in question
+    val toponyms = params.city.split(""",\s*""")
+    // Set of abbreviations for states
     val states_abbrev = states.keySet
+    // Set of full names for states
     val states_full = states.values.toSet
     for (file <- params.input) {
       for (line <- localfh.openr(file)) {
@@ -146,7 +166,7 @@ object FilterDeclaredLocation extends ExperimentApp("FilterDeclaredLocation") {
         val location = force_string(parsed, "user", "location")
         val loc1 = location.toLowerCase.replaceAll("""[^a-z]""", " ").trim
         val locwords = loc1.split("""\s+""")
-        if (locwords.size >= 2 && (cities contains locwords(0))) {
+        if (locwords.size >= 2 && (toponyms contains locwords(0))) {
           var state = ""
           if (states contains locwords(1))
             state = states(locwords(1))
@@ -163,39 +183,48 @@ object FilterDeclaredLocation extends ExperimentApp("FilterDeclaredLocation") {
           }
           if (state.length > 0) {
             val city_state = s"${locwords(0)}, $state"
-            users_cities(user)(city_state) += 1
-            city_counts(city_state) += 1
+            users_cities_tweetcounts_map(user)(city_state) += 1
+            cities_tweetcounts(city_state) += 1
           } else
             errprint("Unrecognized state: %s", location)
         }
       }
     }
-    errprint("Cities counts:")
-    for ((city, count) <- city_counts.toSeq.sortBy(-_._2)) {
+    errprint("Cities tweetcounts:")
+    for ((city, count) <- cities_tweetcounts.toSeq.sortBy(-_._2)) {
       errprint("  %s: %s", city, count)
     }
     errprint("")
     errprint("Users cities:")
-    val user_city_count = mutable.Buffer[(String, String, Int)]()
-    for ((user, cities) <- users_cities) {
+    // List of tuples of (user, city, tweetcount) for users that have only
+    // one city listed.
+    val users_cities_tweetcounts_list = mutable.Buffer[(String, String, Int)]()
+    for ((user, cities) <- users_cities_tweetcounts_map) {
       if (cities.size == 1) {
         val (city, count) = cities.head
-        user_city_count += ((user, city, count))
+        users_cities_tweetcounts_list += ((user, city, count))
       } else {
         errprint("  User %s has %s locations:", user, cities.size)
         for ((city, count) <- cities)
           errprint("    %s = %s", city, count)
       }
     }
+    // Map of cities and counts of users in those cities
     val city_usercount = intmap[String]()
+    // Map of cities and counts of users in those cities with >= 10 tweets
     val city_usercount_10 = intmap[String]()
+    // Map of cities and counts of users in those cities with >= 5 tweets
     val city_usercount_5 = intmap[String]()
-    for ((user, city, count) <- user_city_count.toSeq.sortBy(-_._3)) {
+    // Map of cities and lists of (user, tweetcount) tuples in that city.
+    val cities_users = bufmap[String, (String, Int)]()
+    for ((user, city, count) <- users_cities_tweetcounts_list.toSeq.sortBy(-_._3)) {
       if (count >= 10)
         city_usercount_10(city) += 1
       if (count >= 5)
         city_usercount_5(city) += 1
       city_usercount(city) += 1
+      if (count >= params.min_tweets)
+        cities_users(city) += ((user, count))
       errprint("  User %s: %s = %s", user, city, count)
     }
     errprint("Cities usercounts:")
@@ -209,6 +238,41 @@ object FilterDeclaredLocation extends ExperimentApp("FilterDeclaredLocation") {
     errprint("Cities usercounts with >= 5 tweets:")
     for ((city, count) <- city_usercount_5.toSeq.sortBy(-_._2)) {
       errprint("  %s: %s", city, count)
+    }
+
+    // Select the users for each city
+    val selected_users = mutable.Set[String]()
+    val selected_cities = mutable.Buffer[String]()
+    errprint("Selected users for cities with >= %s tweets:",
+      params.min_tweets)
+    for ((city, users) <- cities_users) {
+      val selected = (new Random).shuffle(users).take(params.max_users)
+      // We only select cities with enough users
+      if (selected.size == params.max_users &&
+          !(excluded_cities contains city)) {
+        selected_users ++= selected.map(_._1)
+        selected_cities += city
+      }
+      errprint("  City %s, %s users, %.2f average tweets/user:",
+        city, selected.size, selected.map(_._2).sum.toDouble / selected.size)
+      for (((user, tweetcount), index) <- (selected zip Stream.from(1)))
+        errprint("    #%s: %s = %s", index, user, tweetcount)
+    }
+    errprint("Selected cities:")
+    for (city <- selected_cities)
+      errprint("  %s", city)
+
+    // Now go through the tweets again and select only those users we want
+    if (params.filtered_tweet_file != null) {
+      val outfile = localfh.openw(params.filtered_tweet_file)
+      for (file <- params.input) {
+        for (line <- localfh.openr(file)) {
+          val parsed = liftweb.json.parse(line)
+          val user = (parsed \ "user" \ "screen_name" values).toString
+          if (selected_users contains user)
+            outfile.println(line)
+        }
+      }
     }
     0
   }
